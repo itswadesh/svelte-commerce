402d9525541b3001499e5c6d61ab23aa7223d019
Swadesh Behera <itswadesh@gmail.com>
2020-02-22 07:26:54 +0530

    fix: settings data

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,37 +1,42 @@
 import { get, post } from "./../lib/api";
 async function authenticationMiddleware(req, res, next) {
-    let user = null,
-        cart = null,
-        settings = null
-    const cookies = require('cookie-universal')(req, res);
-    if (cookies.get('token')) {
-        try {
-            user = await get("users/me", null, cookies.get('token'));
-        } catch (e) {
-            console.log('err at users', e.toString());
-        }
-        req.user = user
-        req.token = cookies.get('token')
-    } else {
-        req.user = {}
-        req.token = null
-        cookies.set('token', null)
-    }
+  let user = null,
+    cart = null,
+    settings = null;
+  const cookies = require("cookie-universal")(req, res);
+  if (cookies.get("token")) {
     try {
-        cart = await get("cart", null, cookies.get('token'), `guest=${cookies.get('guest')}`);
+      user = await get("users/me", null, cookies.get("token"));
     } catch (e) {
-        req.user = {}
-        req.token = null
-        cookies.set('token', null)
-        console.log('err at cart', e.toString());
+      console.log("err at users", e.toString());
     }
-    req.cart = cart
-    try {
-        settings = await get("settings", null, cookies.get('token'));
-    } catch (e) {
-        console.log('err at settings', e.toString());
-    }
-    req.settings = settings
-    next();
+    req.user = user;
+    req.token = cookies.get("token");
+  } else {
+    req.user = {};
+    req.token = null;
+    cookies.set("token", null);
+  }
+  try {
+    cart = await get(
+      "cart",
+      null,
+      cookies.get("token"),
+      `guest=${cookies.get("guest")}`
+    );
+  } catch (e) {
+    req.user = {};
+    req.token = null;
+    cookies.set("token", null);
+    console.log("err at cart", e.toString());
+  }
+  req.cart = cart;
+  try {
+    settings = (await get("settings", null, cookies.get("token"))).data;
+  } catch (e) {
+    console.log("err at settings", e.toString());
+  }
+  req.settings = settings;
+  next();
 }
-export { authenticationMiddleware }
+export { authenticationMiddleware };
--- src/server.js
+++ src/server.js
@@ -1,35 +1,44 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
-import { authenticationMiddleware } from './lib/auth'
-import { apiUrl } from './config'
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env' });
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
+import { authenticationMiddleware } from "./lib/auth";
+import { apiUrl } from "./config";
+const dotenv = require("dotenv").config();
 const { PORT, NODE_ENV, API_URL } = process.env;
 
-const proxy = require('http-proxy-middleware');
-const apiProxy = proxy('/api', { target: API_URL || apiUrl, changeOrigin: true });
-const imgProxy = proxy('/images', { target: API_URL || apiUrl, changeOrigin: true });
-const sitemapProxy = proxy('/sitemap-hs.xml', { target: API_URL || apiUrl, changeOrigin: true });
+const proxy = require("http-proxy-middleware");
+const apiProxy = proxy("/api", {
+  target: API_URL || apiUrl,
+  changeOrigin: true
+});
+const imgProxy = proxy("/images", {
+  target: API_URL || apiUrl,
+  changeOrigin: true
+});
+const sitemapProxy = proxy("/sitemap-hs.xml", {
+  target: API_URL || apiUrl,
+  changeOrigin: true
+});
 
-
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 polka()
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		apiProxy, imgProxy, sitemapProxy,
-		authenticationMiddleware,
-		sapper.middleware({
-			session: (req, res) => ({
-				user: req.user || {},
-				token: req.token,
-				cart: req.cart || {},
-				settings: req.settings || {}
-			})
-		})
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    apiProxy,
+    imgProxy,
+    sitemapProxy,
+    authenticationMiddleware,
+    sapper.middleware({
+      session: (req, res) => ({
+        user: req.user || {},
+        token: req.token,
+        cart: req.cart || {},
+        settings: req.settings || {}
+      })
+    })
+  )
+  .listen(PORT, err => {
+    if (err) console.log("error", err);
+  });
