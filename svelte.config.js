fa5057ff974f5ca28ed7dc7475be373f06645fa9
Swadesh Behera <itswadesh@gmail.com>
2021-07-22 16:55:36 +0530

    prepare for netlify deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
+import preprocess from 'svelte-preprocess'
 import node from '@sveltejs/adapter-node'
+import netlify from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-static'
 
 const config = {
 	kit: {
-		adapter: {
-			adapt() {
-				node()
-			},
-		},
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
 		vite: {
 			server: {
 				proxy: {
