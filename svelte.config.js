516f1a9fc079db06166f3f4a36b0106fb5bc1c1c
Swadesh Behera <itswadesh@gmail.com>
2022-08-21 18:19:11 +0530

    -

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import houdini from 'houdini/preprocess'
 import path from 'path'
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-auto'
-import watchAndRun from '@kitql/vite-plugin-watch-and-run'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
