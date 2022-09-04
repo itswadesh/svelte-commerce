d0fc728cff4f8925bf805967ae2d79f17ded6644
Swadesh Behera <itswadesh@gmail.com>
2022-09-04 14:27:27 +0530

    Coolify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
