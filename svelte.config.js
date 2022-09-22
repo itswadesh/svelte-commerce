f9f63607b7fedeebc0ba9c1ebeafa61bb8f253c9
Swadesh <itswadesh@gmail.com>
2022-09-22 09:20:36 +0530

    fix: redirect

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
