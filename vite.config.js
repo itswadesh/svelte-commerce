ed167e349038b98b3ab66da92a7f5e0030caa2aa
Swadesh Behera <itswadesh@gmail.com>
2022-10-04 20:05:58 +0530

    env example added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-const PUBLIC_HTTP_ENDPOINT = 'http://localhost:7000'
+const PUBLIC_HTTP_ENDPOINT = 'https://staging.zapvi.in'
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
