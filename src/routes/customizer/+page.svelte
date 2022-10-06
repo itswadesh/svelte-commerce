ccebce260fe55e9a6cb3e77ab75b410fac7987e8
Swadesh <itswadesh@gmail.com>
2022-10-06 18:08:58 +0530

    canvas from json

--- src/routes/customizer/+page.svelte
+++ src/routes/customizer/+page.svelte
@@ -3,83 +3,73 @@ import { onMount } from 'svelte'
 import Konva from 'konva'
 
 let container
-let json = `{"attrs":{"width":400,"height":400},"className":"Stage","children":[{"attrs":{},"className":"Layer","children":[{"attrs":{"x":71.020453858611,"y":60.88098560536032,"width":100,"height":90,"fill":"red","name":"rect","stroke":"black","draggable":true,"scaleX":1.8809856053602876,"scaleY":1.8809856053602882},"className":"Rect"},{"attrs":{"x":260,"y":160,"width":100,"height":90,"fill":"blue","name":"rect","stroke":"green","draggable":true},"className":"Rect"},{"attrs":{"x":5,"y":5,"fill":"black","text":"x: 71.020453858611\ny: 60.88098560536032\nrotation: 0\nwidth: 100\nheight: 90\nscaleX: 1.8809856053602876\nscaleY: 1.8809856053602882"},"className":"Text"},{"attrs":{},"className":"Transformer"},{"attrs":{},"className":"Transformer"}]}]}`
+let json
 onMount(() => {
-	const stage = new Konva.Stage({
-		container: container,
-		width: 400,
-		height: 400
-	})
+	json = localStorage.getItem('canvas')
+	// const stage = new Konva.Stage({
+	// 	container: container,
+	// 	width: 400,
+	// 	height: 400
+	// })
+	var stage = Konva.Node.create(json, 'container')
 
 	const layer = new Konva.Layer()
-	var rect = new Konva.Rect({
-		x: 160,
-		y: 60,
-		width: 100,
-		height: 90,
-		fill: 'red',
-		name: 'rect',
-		stroke: 'black',
-		draggable: true
-	})
-	layer.add(rect)
+	// var rect = new Konva.Rect({
+	// 	x: 160,
+	// 	y: 60,
+	// 	width: 100,
+	// 	height: 90,
+	// 	fill: 'red',
+	// 	name: 'rect',
+	// 	stroke: 'black',
+	// 	draggable: true
+	// })
+	// layer.add(rect)
 
-	var rect1 = new Konva.Rect({
-		x: 260,
-		y: 160,
-		width: 100,
-		height: 90,
-		fill: 'blue',
-		name: 'rect',
-		stroke: 'green',
-		draggable: true
-	})
-	layer.add(rect1)
+	// var rect1 = new Konva.Rect({
+	// 	x: 260,
+	// 	y: 160,
+	// 	width: 100,
+	// 	height: 90,
+	// 	fill: 'blue',
+	// 	name: 'rect',
+	// 	stroke: 'green',
+	// 	draggable: true
+	// })
+	// layer.add(rect1)
 
-	var text = new Konva.Text({
-		x: 5,
-		y: 5
-	})
-	layer.add(text)
-	updateText()
+	// updateText()
 
 	// create new transformer
 	var tr = new Konva.Transformer()
 	var tr1 = new Konva.Transformer()
 	layer.add(tr)
 	layer.add(tr1)
-	tr.nodes([rect])
-	tr1.nodes([rect1])
+	// tr.nodes([rect])
+	// tr1.nodes([rect1])
 
-	rect.on('transformstart', function () {
-		console.log('transform start')
-	})
+	// rect.on('transformstart', function () {
+	// 	console.log('transform start')
+	// })
 
-	rect.on('dragmove', function () {
-		updateText()
-	})
-	rect.on('transform', function () {
-		updateText()
-		console.log('transform')
-	})
+	// rect.on('transformend', function () {
+	// 	console.log('transform end')
+	// 	json = stage.toJSON()
+	// 	localStorage.setItem('canvas', json)
+	// })
 
-	rect.on('transformend', function () {
-		console.log('transform end')
-		json = stage.toJSON()
-	})
-
-	function updateText() {
-		var lines = [
-			'x: ' + rect.x(),
-			'y: ' + rect.y(),
-			'rotation: ' + rect.rotation(),
-			'width: ' + rect.width(),
-			'height: ' + rect.height(),
-			'scaleX: ' + rect.scaleX(),
-			'scaleY: ' + rect.scaleY()
-		]
-		text.text(lines.join('\n'))
-	}
+	// function updateText() {
+	// 	var lines = [
+	// 		'x: ' + rect.x(),
+	// 		'y: ' + rect.y(),
+	// 		'rotation: ' + rect.rotation(),
+	// 		'width: ' + rect.width(),
+	// 		'height: ' + rect.height(),
+	// 		'scaleX: ' + rect.scaleX(),
+	// 		'scaleY: ' + rect.scaleY()
+	// 	]
+	// 	text.text(lines.join('\n'))
+	// }
 
 	stage.add(layer)
 })
@@ -88,7 +78,7 @@ function toJSON() {
 }
 </script>
 
-<div bind:this="{container}"></div>
+<div bind:this="{container}" id="container"></div>
 <button on:click="{toJSON}">export</button>
 <br />
 <br />
---
+++ src/routes/customizer/stage/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+import { onMount } from 'svelte'
+import Konva from 'konva'
+
+let container
+let json =
+	'{"attrs":{"width":400,"height":400},"className":"Stage","children":[{"attrs":{},"className":"Layer","children":[{"attrs":{"x":71.020453858611,"y":60.88098560536032,"width":100,"height":90,"fill":"red","name":"rect","stroke":"black","draggable":true,"scaleX":1.8809856053602876,"scaleY":1.8809856053602882},"className":"Rect"},{"attrs":{"x":260,"y":160,"width":100,"height":90,"fill":"blue","name":"rect","stroke":"green","draggable":true},"className":"Rect"},{"attrs":{"x":5,"y":5,"fill":"black","text":"x: 71.020453858611\ny: 60.88098560536032\nrotation: 0\nwidth: 100\nheight: 90\nscaleX: 1.8809856053602876\nscaleY: 1.8809856053602882"},"className":"Text"},{"attrs":{},"className":"Transformer"},{"attrs":{},"className":"Transformer"}]}]}'
+// var json =
+// 	'{"attrs":{"width":578,"height":200},"className":"Stage","children":[{"attrs":{},"className":"Layer","children":[{"attrs":{"width":"auto","height":"auto","text":"Text Shadow!","fontFamily":"Calibri","fontSize":95,"x":20,"y":20,"stroke":"red","strokeWidth":2,"shadowColor":"black","shadowBlur":2,"shadowOffsetX":10,"shadowOffsetY":10,"shadowOpacity":0.5},"className":"Text"},{"attrs":{"stroke":"green","strokeWidth":10,"lineJoin":"round","lineCap":"round","points":[{"x":50,"y":140},{"x":450,"y":160}],"shadowColor":"black","shadowBlur":10,"shadowOffsetX":5,"shadowOffsetY":5,"shadowOpacity":0.5},"className":"Line"},{"attrs":{"x":280,"y":100,"width":100,"height":50,"fill":"#00D2FF","stroke":"black","strokeWidth":4,"shadowColor":"black","shadowBlur":10,"shadowOffsetX":5,"shadowOffsetY":5,"shadowOpacity":0.5,"rotation":0.35038503988659,"id":"blueRectangle"},"className":"Rect"},{"attrs":{"x":100,"y":41,"width":106,"height":118,"id":"yodaImage"},"className":"Image"}]}]}'
+onMount(() => {
+	var stage = Konva.Node.create(json, 'container')
+
+	/*
+	 * set functions
+	 */
+	// stage.findOne('#blueRectangle').on('mouseover mouseout', function () {
+	// 	var stroke = this.stroke()
+	// 	this.stroke(stroke === 'black' ? 'red' : 'black')
+	// })
+	// /*
+	//  * set images
+	//  */
+	// var imageObj = new Image()
+	// imageObj.onload = function () {
+	// 	stage.findOne('#yodaImage').image(imageObj)
+	// }
+	// imageObj.src = '/icon.png'
+})
+</script>
+
+<div bind:this="{container}" id="container"></div>
+<!-- <button on:click="{toJSON}">export</button> -->
+<br />
+<br />
+<br />
+<code class="code">
+	{json}
+</code>
