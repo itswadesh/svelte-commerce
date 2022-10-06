ac2f1c176d5a499dfc69e885dd6eb346b85b1f78
Swadesh <itswadesh@gmail.com>
2022-10-06 17:18:45 +0530

    transformer

--- src/routes/customizer/+page.svelte
+++ src/routes/customizer/+page.svelte
@@ -10,15 +10,74 @@ onMount(() => {
 		height: 400
 	})
 	const layer = new Konva.Layer()
-	const simpleText = new Konva.Text({
-		x: stage.width() / 2,
-		y: 15,
-		text: 'Hello Konva',
-		fontSize: 30,
-		fontFamily: 'Calibri',
-		fill: 'green'
+	var rect = new Konva.Rect({
+		x: 160,
+		y: 60,
+		width: 100,
+		height: 90,
+		fill: 'red',
+		name: 'rect',
+		stroke: 'black',
+		draggable: true
 	})
-	layer.add(simpleText)
+	layer.add(rect)
+
+	var rect1 = new Konva.Rect({
+		x: 260,
+		y: 160,
+		width: 100,
+		height: 90,
+		fill: 'blue',
+		name: 'rect',
+		stroke: 'green',
+		draggable: true
+	})
+	layer.add(rect1)
+
+	var text = new Konva.Text({
+		x: 5,
+		y: 5
+	})
+	layer.add(text)
+	updateText()
+
+	// create new transformer
+	var tr = new Konva.Transformer()
+	var tr1 = new Konva.Transformer()
+	layer.add(tr)
+	layer.add(tr1)
+	tr.nodes([rect])
+	tr1.nodes([rect1])
+
+	rect.on('transformstart', function () {
+		console.log('transform start')
+	})
+
+	rect.on('dragmove', function () {
+		updateText()
+	})
+	rect.on('transform', function () {
+		updateText()
+		console.log('transform')
+	})
+
+	rect.on('transformend', function () {
+		console.log('transform end')
+	})
+
+	function updateText() {
+		var lines = [
+			'x: ' + rect.x(),
+			'y: ' + rect.y(),
+			'rotation: ' + rect.rotation(),
+			'width: ' + rect.width(),
+			'height: ' + rect.height(),
+			'scaleX: ' + rect.scaleX(),
+			'scaleY: ' + rect.scaleY()
+		]
+		text.text(lines.join('\n'))
+	}
+
 	stage.add(layer)
 })
 </script>
