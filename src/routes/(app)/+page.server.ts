23ebab14c2866993a69eac3887216b3e56e0da0f
Swadesh Behera <itswadesh@gmail.com>
2022-10-09 22:10:55 +0530

    fix

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -3,12 +3,8 @@ import { error } from '@sveltejs/kit'
 export async function load({ params, query, locals, parent, cookies }) {
 	const { store } = locals
 	const home = await gett(`home?store=${store?.id}`)
-
-
 	if (home) {
-		return { home: home }	
+		return { home: home }
 	}
-
-
 	throw error(500, 'Internal Server Error')
 }
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -8,8 +8,6 @@ import PickedBanners from '$lib/home/PickedBanners.svelte'
 import LazyImg from '$lib/components/Image/LazyImg.svelte'
 import MobileFooter from '$lib/MobileFooter.svelte'
 import DummyProductCard from '$lib/DummyProductCard.svelte'
-import { browser } from '$app/environment'
-
 
 export let data
 
@@ -36,14 +34,6 @@ const seoProps = {
 		url: '/logo.svg'
 	}
 }
-if(browser){
-	const homedata = JSON.stringify(data);
-	localStorage.setItem('homedata', homedata);
-	if(homedata){
-		data = JSON.parse(homedata);
-	}
-} 
-
 
 $: heroBanners = data.home?.banners?.data.filter((b) => {
 	return b.type === 'hero'
