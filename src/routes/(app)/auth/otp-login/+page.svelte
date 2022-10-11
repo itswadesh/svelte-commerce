e40f6bbfac7fe780b6451874c0ee11ac0bcbd353
Swadesh Behera <itswadesh@gmail.com>
2022-10-11 23:22:15 +0530

    fix: login redirect

--- src/routes/(app)/auth/otp-login/+page.svelte
+++ src/routes/(app)/auth/otp-login/+page.svelte
@@ -28,18 +28,21 @@ const seoProps = {
 	description: 'OTP Login'
 }
 
+// console.log('zzzzzzzzzzzzzzzzzz', $page)
+
 onMount(async () => {})
 
-let phone,
-	loading = false,
-	otpRequestSend = false,
-	resendAfter = 0
+let phone
+let loading = false
+let otpRequestSend = false
+let resendAfter = 0
+let ref = $page?.url?.searchParams.get('ref')
 
 async function handleSendOTP({ detail }) {
 	phone = detail
 	try {
 		loading = true
-		const data = await post('get-otp', { phone })
+		const data = await post('get-otp', { phone }, $page.data.origin)
 		resendAfter = data?.timer
 		otpRequestSend = true
 	} catch (e) {
@@ -53,7 +56,7 @@ async function handleVerifyOtp({ detail }) {
 	try {
 		loading = true
 		const otp = detail
-		const data = await post('verify-otp', { phone, otp })
+		const data = await post('verify-otp', { phone, otp }, $page.data.origin)
 		const me = {
 			email: data.email,
 			phone: data.phone,
@@ -67,7 +70,7 @@ async function handleVerifyOtp({ detail }) {
 		await cookies.set('me', me, { path: '/' })
 		// $page.data.me = me
 		await invalidateAll()
-		let r = data.ref || '/'
+		let r = ref || '/'
 		if (browser) goto(r)
 	} catch (e) {
 		toast(e, 'error')
@@ -103,6 +106,16 @@ function changeNumber() {
 			on:resend="{({ detail }) => handleSendOTP({ detail })}"
 			on:changeNumber="{changeNumber}" />
 	{/if}
+
+	<!-- <div
+		class="mx-auto mb-5 flex w-full max-w-sm flex-wrap items-center justify-center gap-4 leading-4">
+		<a
+			href="{`/auth/login?ref=${$page.url.searchParams.get('ref') || '/'}`}"
+			aria-label="Click to login with email"
+			class="max-w-max whitespace-nowrap text-primary-500 hover:text-primary-700 hover:underline hover:underline">
+			Login with email
+		</a>
+	</div> -->
 
 	<p class="text-center text-sm text-gray-500">
 		By clicking send OTP you are accepting our
