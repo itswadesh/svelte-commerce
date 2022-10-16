a93d92d6f39b40b9821274700dfaf5abe6ba2606
Swadesh Behera <itswadesh@gmail.com>
2022-10-16 19:37:25 +0530

    fix: SEO issue at product card

--- src/lib/DummyProductCard.svelte
+++ src/lib/DummyProductCard.svelte
@@ -1,3 +1,1 @@
-<div class="col-span-1 block w-full bg-gray-500 opacity-0 sm:w-48 sm:flex-shrink-0">
-	Dummy Product Card
-</div>
+<div class="col-span-1 block w-full bg-gray-500 opacity-0 sm:w-48 sm:flex-shrink-0">...</div>
