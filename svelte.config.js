4eb5e5deda0199bd3db377172a67e129ac6babbf
Swadesh Behera <itswadesh@gmail.com>
2023-01-20 22:30:14 +0530

    enabled edge function

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
+// import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +11,7 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ edge: true }),
 		csrf: {
 			checkOrigin: false
 		}
