921a63cc41f2910f5161ef91539481e9b083545c
Swadesh Behera <itswadesh@gmail.com>
2023-01-20 22:15:20 +0530

    fix: WooCommerceApi

--- src/lib/utils/server.ts
+++ src/lib/utils/server.ts
@@ -1,7 +1,8 @@
 import cookie from 'cookie'
 import { BIG_COMMERCE_BASE_URL, HTTP_ENDPOINT, bigcommerceHeaders, provider } from '../config'
 
-import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api'
+import pkg from '@woocommerce/woocommerce-rest-api'
+const WooCommerceRestApi = pkg.default
 import { WOO_COMMERCE_STORE_LINK, WOO_COMMERCE_KEY, WOO_COMMERCE_SECRET } from '../config'
 import { serialize } from '.'
 
