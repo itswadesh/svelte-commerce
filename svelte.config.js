45dd70fa20d421c7e28d0c1e0fabb3bbfd5fbac3
Swadesh Behera <itswadesh@gmail.com>
2023-01-20 22:38:24 +0530

    -

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-vercel'
-// import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
