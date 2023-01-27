dc4115611eb63bbe605c23089dbb050d9a43044e
Swadesh Behera <itswadesh@gmail.com>
2023-01-27 14:28:06 +0530

    removeNullish, buildQueryFromObject

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -202,3 +202,15 @@ export const mapWoocommerceProducts = (p) => {
 		return {}
 	}
 }
+
+export const removeNullish = (obj) =>
+	Object.entries(obj).reduce((a, [k, v]) => (v ? ((a[k] = v), a) : a), {})
+
+export const buildQueryFromObject = (search, prefix = '') =>
+	Object.entries(search)
+		.map(([key, value]) =>
+			typeof value === 'object'
+				? buildQueryFromObject(value, key)
+				: `${prefix ? `${prefix}[${key}]` : `${key}`}=${value}`
+		)
+		.join('&')
