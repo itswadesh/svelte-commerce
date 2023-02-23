5ace44db22c8add662c0e64626634b03ffeb9fae
pratapmaharana <pratapofficial1111@gmail.com>
2023-02-23 20:16:13 +0530

    Fixed: Single image upload

--- src/lib/SingleImageUpload.svelte
+++ src/lib/SingleImageUpload.svelte
@@ -14,30 +14,30 @@
 }
 </style>
 
-<script lang="ts">
+<script>
 import { createEventDispatcher } from 'svelte'
 import { toast } from './utils'
 
 const dispatch = createEventDispatcher()
 
-export let images: string[] = [],
+export let images,
 	title = 'Upload File',
 	folder = 'img',
-	maxSize = 10 * 1000 * 1000,
+	maxSize = null,
 	multi = false,
 	avatar = false,
 	accept = 'image/*',
 	loading = false
 
 let uploading = false,
-	files: string[] = [],
+	files,
 	deleting = false
 
 let clazz = ''
 
 export { clazz as class }
 
-async function remove(img: string[]) {
+async function remove(img) {
 	try {
 		deleting = true
 		const response = await fetch('/server/files/delete', {
@@ -56,7 +56,7 @@ async function remove(img: string[]) {
 	}
 }
 
-const handleSubmit = async (e: any) => {
+const handleSubmit = async (e) => {
 	var isOk = true
 	const maxSize = 10 * 1000 * 1000 // 2MB
 	const file = e //e.target.files[0]
@@ -83,7 +83,7 @@ const handleSubmit = async (e: any) => {
 
 <div
 	class="{clazz} group relative h-36 w-36 shrink-0 grow-0 bg-gray-50 transition duration-300 hover:bg-white">
-	{#if !uploading && !deleting && images !== undefined}
+	{#if !uploading && !deleting && images != '' && images !== undefined}
 		<div
 			class="slowtransition absolute inset-0 z-0 hidden items-center justify-center bg-black bg-opacity-50 text-center group-hover:flex">
 			<span class="font-medium text-white">Click to Update</span>
@@ -124,24 +124,18 @@ const handleSubmit = async (e: any) => {
 					{/if}
 				</div>
 			</div>
-		{:else if images != undefined}
+		{:else if images != '' && images != undefined}
 			<div class="h-36 w-36 border-2 border-gray-300 bg-white text-xs">
-				<embed
-					src="{images[0]}"
-					frameBorder="{0}"
-					scrolling="auto"
-					height="100%"
-					width="100%"
-					class="object-contain object-center" />
+				<img src="{images}" alt="images" class="h-full w-full object-contain object-center" />
 			</div>
 		{:else if avatar}
 			<div
-				class="flex h-36 w-36 items-center justify-center border-2 border-gray-300 bg-gray-100 p-1">
+				class="flex items-center justify-center h-36 w-36 border-2 p-1 border-gray-300 bg-gray-100">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 24 24"
 					fill="currentColor"
-					class="h-full w-full text-gray-300">
+					class="w-full h-full text-gray-300">
 					<path
 						fill-rule="evenodd"
 						d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
