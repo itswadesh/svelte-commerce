246d39bb3314f1035925456d7ea21ff2a3416840
pratapmaharana <pratapofficial1111@gmail.com>
2023-02-28 17:42:37 +0530

    Fix: Pagination

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -12,11 +12,11 @@ import { page } from '$app/stores'
 
 // const dispatch = createEventDispatcher()
 
-export let count = 1,
+export let count = 10,
 	current = 1
 
 count = +count
-$: pages = +count > 100 ? 100 : +count
+$: pages = +count
 $: startTab = 5 - current <= 5 && 5 - current >= 0 ? 1 : current - 4
 $: endTab = startTab + 9
 function changePage(e) {
@@ -40,64 +40,57 @@ function changePage(e) {
 </script>
 
 {#if count > 1}
-	<div class="items-center justify-between border-t border-gray-200 lg:flex lg:pt-5">
-		<div class="items-center whitespace-nowrap py-4 text-xs text-gray-500 sm:text-base">
+	<div
+		class="flex flex-wrap items-center justify-between gap-4 border-t border-gray-300 pt-4 text-sm">
+		<div class="whitespace-nowrap text-gray-500">
 			Page {current} of {count}
 		</div>
 
-		<div class="flex items-center justify-center gap-2 text-center xl:mx-2">
+		<div class="flex w-full max-w-max flex-wrap items-center gap-2">
 			{#if +current > 1}
 				<button
-					class="inline-flex items-center rounded border border-gray-300 p-0.5 font-bold tracking-wide text-gray-700 focus:outline-none hover:border-gray-800 sm:p-2 lg:py-2 lg:px-4"
-					on:click="{() => changePage(+current - 1)}"
-				>
+					class="flex h-8 items-center justify-center gap-2 rounded-md border border-gray-300 px-3 focus:outline-none hover:border-gray-800"
+					on:click="{() => changePage(+current - 1)}">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						class="h-4 w-4 sm:mr-2"
 						fill="none"
 						viewBox="0 0 24 24"
+						stroke-width="1.5"
 						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M15 19l-7-7 7-7"></path>
+						class="h-4 w-4">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5"
+						></path>
 					</svg>
 
-					<span class="hidden text-xs sm:block">Previous</span>
+					<span class="hidden sm:block">Previous</span>
 				</button>
 			{/if}
 
-			<div class="flex flex-wrap gap-1">
-				{#each { length: pages } as _, i}
-					{#if startTab <= i + 1 && endTab - 1 >= i}
-						<button
-							class="mx-auto h-5 w-5 rounded border border-transparent text-xs focus:outline-none hover:border-gray-800 sm:h-8 sm:w-8 sm:text-base"
-							class:active="{+current === i + 1}"
-							on:click="{() => changePage(i + 1)}"
-						>
-							{i + 1}
-						</button>
-					{/if}
-				{/each}
-			</div>
+			{#each { length: pages } as _, i}
+				{#if startTab <= i + 1 && endTab - 1 >= i}
+					<button
+						class="h-8 w-8 rounded border-gray-800 focus:outline-none hover:border hover:border-gray-800"
+						class:active="{+current === i + 1}"
+						on:click="{() => changePage(i + 1)}">
+						{i + 1}
+					</button>
+				{/if}
+			{/each}
 
 			{#if +current < count}
 				<button
-					class="inline-flex items-center rounded border border-gray-300 p-0.5 text-center font-bold tracking-wide text-gray-700 focus:outline-none  hover:border-gray-800 sm:p-2 lg:py-2 lg:px-4"
-					on:click="{() => changePage(+current + 1)}"
-				>
-					<span class="hidden text-xs sm:block">Next</span>
+					class="flex h-8 items-center justify-center gap-2 rounded-md border border-gray-300 px-3 focus:outline-none hover:border-gray-800"
+					on:click="{() => changePage(+current + 1)}">
+					<span class="hidden sm:block">Next</span>
 
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						class="h-4 w-4 sm:ml-2"
 						fill="none"
 						viewBox="0 0 24 24"
+						stroke-width="1.5"
 						stroke="currentColor"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"
+						class="h-4 w-4">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"
 						></path>
 					</svg>
 				</button>
