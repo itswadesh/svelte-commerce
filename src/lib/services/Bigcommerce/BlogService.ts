fed8935f58d1676f3769ebfe6d848b70e86a1262
Swadesh Behera <itswadesh@gmail.com>
2023-03-14 21:29:04 +0530

    fix: filenaming for Bigcommerce

---
+++ src/lib/services/Bigcommerce/BlogService.ts
@@ -0,0 +1,42 @@
+import { provider } from '$lib/config'
+import type { Error } from '$lib/types'
+import { getAPI } from '$lib/utils/api'
+import { getBySid, getBigcommerceApi, getWoocommerceApi } from '$lib/utils/server'
+import { serializeNonPOJOs } from '$lib/utils/validations'
+import { error } from '@sveltejs/kit'
+
+export const fetchBlogs = async ({ origin, storeId, server = false, sid = null }: any) => {
+	try {
+		let res: any = {}
+
+		res = await getBigcommerceApi(`customers/me`, {}, sid)
+
+		return res.data || []
+	} catch (e) {
+		throw error(e.status, e.data?.message || e.message)
+	}
+}
+
+export const fetchLatestBlogs = async ({ origin, storeId, server = false, sid = null }: any) => {
+	try {
+		let res: any = {}
+
+		res = await getBigcommerceApi(`customers/me`, {}, sid)
+
+		return res.data || []
+	} catch (e) {
+		throw error(e.status, e.data?.message || e.message)
+	}
+}
+
+export const fetchBlog = async ({ origin, id, storeId, server = false, sid = null }: any) => {
+	try {
+		let res: any = {}
+
+		res = await getBigcommerceApi(`customers/me`, {}, sid)
+
+		return res.data || []
+	} catch (e) {
+		throw error(e.status, e.data?.message || e.message)
+	}
+}
