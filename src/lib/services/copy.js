f7b0217c77e23e3c14eb332a159362b43f25a354
Swadesh Behera <itswadesh@gmail.com>
2023-04-07 08:41:43 +0530

    Auto Sync of Active service vs Active directory

--- src/lib/services/copy.js
+++ src/lib/services/copy.js
@@ -6,7 +6,7 @@ config()
 const provider = process.env.PUBLIC_API_PROVIDER || 'Litekart'
 
 shell.cd('src/lib/services')
-shell.ln('-sf', `${provider}/`, `Active/`)
+shell.ln('-s', `src/lib/services/${provider}/`, `src/lib/services/Active/`)
 // shell.rm('-Rf', 'Active')
 // shell.mkdir('-p', '-f', 'Active')
 // shell.cp('-R', `${provider}/AddressService.ts`, 'Active/AddressService.ts')
