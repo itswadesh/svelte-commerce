acc1c4c6eb6f4e986707e80b72f39be8096d8029
Swadesh Behera <itswadesh@gmail.com>
2023-04-19 18:47:22 +0530

    fix: toggle wishlist after login

--- src/routes/(app)/my/wishlist/add/[pid]/+page.server.ts
+++ src/routes/(app)/my/wishlist/add/[pid]/+page.server.ts
@@ -1,23 +1,25 @@
 import { WishlistService } from '$lib/services'
-
 import { redirect } from '@sveltejs/kit'
 
 export async function load({ locals, cookies, params, request }) {
 	const pid = params.pid
+	const storeId = locals.store?.id
+	const sid = locals.sid
 	const isExistInWishlist = await WishlistService.checkWishlist({
 		pid,
 		vid: pid,
 		storeId: locals.store?.id,
 		server: true,
-		sid: cookies.get('connect.sid')
+		sid
 	})
 
 	if (!isExistInWishlist) {
 		await WishlistService.toggleWishlistService({
 			pid: pid,
 			vid: pid,
-			storeId: locals.store?.id,
-			sid: cookies.get('connect.sid')
+			storeId,
+			sid,
+			server: true
 		})
 	}
 	throw redirect(307, '/my/wishlist')
