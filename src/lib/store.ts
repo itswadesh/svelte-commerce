81dd029182a793d7d7103580b4f5608df8134cbc
Swadesh Behera <itswadesh@gmail.com>
2023-04-22 14:15:30 +0530

    fix: loginUrl export

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -15,39 +15,39 @@ export const geoLocation = writable({
 // export const settings = writable({})
 // export const store = writable({})
 // export const megamenu = writable({})
-// export const loginUrl = writable('/auth/login')
+export const loginUrl = writable('/auth/login')
 // export const me = writable({})
 export const loadingDelayed = writable(false)
 
-export const fetchLocation = async () => {
-	try {
-		const geo = cookies.get('geo')
-		const geo1: any = {}
-		geo1.lat = geo.lat
-		geo1.lng = geo.lng
-		geo1.selectedPrediction = decodeURIComponent(geo.selectedPrediction)
-		if (geo1.selectedPrediction === 'undefined') geo1.selectedPrediction = null
-		geo1.locality = decodeURIComponent(geo.locality)
-		if (geo1.locality === 'undefined') geo1.locality = null
-		geo1.city = decodeURIComponent(geo.city)
-		if (geo1.city === 'undefined') geo1.city = null
-		geo1.state = decodeURIComponent(geo.state)
-		if (geo1.state === 'undefined') geo1.state = null
-		geo1.country = decodeURIComponent(geo.country)
-		if (geo1.country === 'undefined') geo1.country = null
-		geoLocation.set({ ...geo1 })
-	} catch (e) {
-		geoLocation.set({
-			selectedPrediction: null,
-			locality: null,
-			city: null,
-			state: null,
-			country: null,
-			lat: 0,
-			lng: 0
-		})
-	}
-}
+// export const fetchLocation = async () => {
+// 	try {
+// 		const geo = cookies.get('geo')
+// 		const geo1: any = {}
+// 		geo1.lat = geo.lat
+// 		geo1.lng = geo.lng
+// 		geo1.selectedPrediction = decodeURIComponent(geo.selectedPrediction)
+// 		if (geo1.selectedPrediction === 'undefined') geo1.selectedPrediction = null
+// 		geo1.locality = decodeURIComponent(geo.locality)
+// 		if (geo1.locality === 'undefined') geo1.locality = null
+// 		geo1.city = decodeURIComponent(geo.city)
+// 		if (geo1.city === 'undefined') geo1.city = null
+// 		geo1.state = decodeURIComponent(geo.state)
+// 		if (geo1.state === 'undefined') geo1.state = null
+// 		geo1.country = decodeURIComponent(geo.country)
+// 		if (geo1.country === 'undefined') geo1.country = null
+// 		geoLocation.set({ ...geo1 })
+// 	} catch (e) {
+// 		geoLocation.set({
+// 			selectedPrediction: null,
+// 			locality: null,
+// 			city: null,
+// 			state: null,
+// 			country: null,
+// 			lat: 0,
+// 			lng: 0
+// 		})
+// 	}
+// }
 // export const fetchSettings = async () => {
 // 	try {
 // 		const data = (await getBySid('settings')).data?.settings
