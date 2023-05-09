7b6c6ab2c4a539cd93ee8c38e800a9fd4cc4e5d2
Swadesh Behera <itswadesh@gmail.com>
2023-05-09 19:12:34 +0530

    fix: store not found issue for medusa

--- src/lib/services/woocommerce/init-service.ts
+++ src/lib/services/woocommerce/init-service.ts
@@ -1,13 +1,4 @@
-import { error } from '@sveltejs/kit'
-import { DOMAIN } from '$lib/config'
-
-export const fetchInit = async (host) => {
+export const fetchInit = async () => {
 	// This is called once during hard reload + everytime footer is hit through /server/store/server.ts
-	try {
-		let res: any = {}
-		// DOMAIN value is proviede in case of self hosted and host value in case of SaaS
-		return res || {}
-	} catch (e) {
-		throw error(e.status, e.data?.message || e.message)
-	}
+	return { storeOne: { _id: 'Woocommerce' } }
 }
