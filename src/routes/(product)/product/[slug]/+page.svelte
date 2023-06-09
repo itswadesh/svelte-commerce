ee3d3d61ac0d5eea45a1b05d7f711c82a25c439c
pratapmaharana <pratapofficial1111@gmail.com>
2023-06-09 14:23:51 +0530

    add: seller information at proudct details page

--- src/routes/(product)/product/[slug]/+page.svelte
+++ src/routes/(product)/product/[slug]/+page.svelte
@@ -592,7 +592,17 @@ function handleMobileCanvas() {
 							{/if}
 						</div>
 
-						<p class="text-sm font-semibold text-green-700">Inclusive of all taxes</p>
+						<p class="mb-2 text-sm font-semibold text-green-700">Inclusive of all taxes</p>
+					{/if}
+
+					{#if data?.product?.vendor && data?.product?.vendor?.slug && data?.product?.vendor?.name}
+						<div class="mb-5 flex items-center gap-2">
+							<span class="font-semibold"> Seller : </span>
+
+							<a href="/vendor/{data?.product?.vendor?.slug}" class="hover:underline">
+								{data?.product?.vendor?.name}
+							</a>
+						</div>
 					{/if}
 				</div>
 
@@ -629,33 +639,35 @@ function handleMobileCanvas() {
 
 				<!-- Delivery Options Mobile -->
 
-				<div class="mb-5 sm:hidden block">
-					<h6 class="mb-2 flex items-center gap-2 font-semibold uppercase">
-						<span> Delivery Options </span>
+				{#if data.product?.deliveryDetails || $page.data.store?.isIndianPincodes}
+					<div class="mb-5 sm:hidden block">
+						<h6 class="mb-2 flex items-center gap-2 font-semibold uppercase">
+							<span> Delivery Options </span>
 
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1"
-							stroke="currentColor"
-							class="h-5 w-5">
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-							></path>
-						</svg>
-					</h6>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1"
+								stroke="currentColor"
+								class="h-5 w-5">
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+								></path>
+							</svg>
+						</h6>
 
-					{#if data.product?.deliveryDetails}
-						<p class="text-sm">
-							{data.product?.deliveryDetails}
-						</p>
-					{:else if $page.data.store?.isIndianPincodes}
-						<DeliveryOptions product="{data.product}" deliveryDetails="{data.deliveryDetails}" />
-					{/if}
-				</div>
+						{#if data.product?.deliveryDetails}
+							<p class="text-sm">
+								{data.product?.deliveryDetails}
+							</p>
+						{:else if $page.data.store?.isIndianPincodes}
+							<DeliveryOptions product="{data.product}" deliveryDetails="{data.deliveryDetails}" />
+						{/if}
+					</div>
+				{/if}
 
 				<!-- prices desktop -->
 
@@ -686,7 +698,17 @@ function handleMobileCanvas() {
 							{/if}
 						</div>
 
-						<p class="mb-5 text-sm font-semibold text-green-700">Inclusive of all taxes</p>
+						<p class="mb-2 text-sm font-semibold text-green-700">Inclusive of all taxes</p>
+					{/if}
+
+					{#if data?.product?.vendor && data?.product?.vendor?.slug && data?.product?.vendor?.name}
+						<div class="mb-5 flex items-center gap-2">
+							<span class="font-semibold"> Seller : </span>
+
+							<a href="/vendor/{data?.product?.vendor?.slug}" class="hover:underline">
+								{data?.product?.vendor?.name}
+							</a>
+						</div>
 					{/if}
 				</div>
 
@@ -1226,33 +1248,35 @@ function handleMobileCanvas() {
 
 				<!-- Delivery Options Desktop -->
 
-				<div class="mb-5 hidden sm:block">
-					<h6 class="mb-2 flex items-center gap-2 font-semibold uppercase">
-						<span> Delivery Options </span>
+				{#if data.product?.deliveryDetails || $page.data.store?.isIndianPincodes}
+					<div class="mb-5 hidden sm:block">
+						<h6 class="mb-2 flex items-center gap-2 font-semibold uppercase">
+							<span> Delivery Options </span>
 
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1"
-							stroke="currentColor"
-							class="h-5 w-5">
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-							></path>
-						</svg>
-					</h6>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1"
+								stroke="currentColor"
+								class="h-5 w-5">
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+								></path>
+							</svg>
+						</h6>
 
-					{#if data.product?.deliveryDetails}
-						<p class="text-sm">
-							{data.product?.deliveryDetails}
-						</p>
-					{:else if $page.data.store?.isIndianPincodes}
-						<DeliveryOptions product="{data.product}" deliveryDetails="{data.deliveryDetails}" />
-					{/if}
-				</div>
+						{#if data.product?.deliveryDetails}
+							<p class="text-sm">
+								{data.product?.deliveryDetails}
+							</p>
+						{:else if $page.data.store?.isIndianPincodes}
+							<DeliveryOptions product="{data.product}" deliveryDetails="{data.deliveryDetails}" />
+						{/if}
+					</div>
+				{/if}
 
 				<!-- Ratings & Reviews -->
 
@@ -1308,6 +1332,8 @@ function handleMobileCanvas() {
 						{error?.message}
 					{/await}
 				{/if}
+
+				<!-- Promo video -->
 
 				{#if $page.data.store?.storePromoVideo?.active?.val}
 					<iframe
--- src/routes/(product)/product/[slug]/_CategoryPoolButtons.svelte
+++ src/routes/(product)/product/[slug]/_CategoryPoolButtons.svelte
@@ -1,29 +1,33 @@
 <script>
 export let categoryPool = []
+
+// console.log('categoryPool', categoryPool)
 </script>
 
 {#if categoryPool?.length}
 	<ul
 		class="w-[89vw] sm:w-full flex sm:flex-wrap items-center justify-start sm:justify-center gap-2 sm:gap-4 overflow-x-auto sm:overflow-x-visible scrollbar-none">
 		{#each categoryPool as c}
-			<li>
-				<a
-					href="/{c.slug}"
-					class="py-2 px-6 text-sm rounded-full font-semibold border bg-transparent border-zinc-200 hover:border-primary-500 text-primary-500 flex items-center justify-center gap-1 uppercase whitespace-nowrap transition duration-300 focus:outline-none">
-					<span>{c.name}</span>
+			{#if c}
+				<li>
+					<a
+						href="/{c.slug}"
+						class="py-2 px-6 text-sm rounded-full font-semibold border bg-transparent border-zinc-200 hover:border-primary-500 text-primary-500 flex items-center justify-center gap-1 uppercase whitespace-nowrap transition duration-300 focus:outline-none">
+						<span>{c.name}</span>
 
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						class="w-5 h-5">
-						<path
-							fill-rule="evenodd"
-							d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-							clip-rule="evenodd"></path>
-					</svg>
-				</a>
-			</li>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="w-5 h-5">
+							<path
+								fill-rule="evenodd"
+								d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+								clip-rule="evenodd"></path>
+						</svg>
+					</a>
+				</li>
+			{/if}
 		{/each}
 	</ul>
 {/if}
