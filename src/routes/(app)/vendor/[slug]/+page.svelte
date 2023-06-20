a24196e35aed7fea4363bb301dcb47ee428433cd
pratapmaharana <pratapofficial1111@gmail.com>
2023-06-20 16:41:19 +0530

    -

--- src/routes/(app)/vendor/[slug]/+page.svelte
+++ src/routes/(app)/vendor/[slug]/+page.svelte
@@ -282,26 +282,9 @@ const seoProps = {
 			<!-- About Vendor -->
 
 			{#if data.vendor?.description}
-				<div class="mb-10">
-					<h5 class="mb-2 flex items-center gap-2 font-semibold sm:mb-4 sm:text-lg">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-4 w-4 sm:h-5 sm:w-5"
-							viewBox="0 0 20 20"
-							fill="currentColor">
-							<path
-								fill-rule="evenodd"
-								d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-								clip-rule="evenodd"></path>
-						</svg>
-
-						<span>About Vendor</span>
-					</h5>
-
-					<p class="prose prose-sm sm:prose-base max-w-none">
-						{@html data.vendor?.description}
-					</p>
-				</div>
+				<p class="mb-10 prose prose-sm sm:prose-base max-w-none">
+					{@html data.vendor?.description}
+				</p>
 			{/if}
 
 			<!-- Categories -->
