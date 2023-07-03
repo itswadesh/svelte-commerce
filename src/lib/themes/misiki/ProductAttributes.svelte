14f465f567ff53a8620634b1787ba46cc0112250
pratapmaharana <pratapofficial1111@gmail.com>
2023-07-03 20:22:31 +0530

    -

--- src/lib/themes/misiki/ProductAttributes.svelte
+++ src/lib/themes/misiki/ProductAttributes.svelte
@@ -6,11 +6,11 @@ export let attributes = []
 	<div class="mb-5">
 		<h6 class="mb-2 flex items-center gap-2 font-semibold uppercase">Specifications</h6>
 
-		<ul class="m-0 p-0 list-none grid grid-cols-2 gap-4">
+		<ul class="m-0 p-0 list-none grid grid-cols-2 gap-x-10 gap-y-4">
 			{#each attributes as att}
 				{#if att.key && att.val}
 					<li class="col-span-1 flex flex-col gap-1">
-						<h6 class="text-xs">
+						<h6 class="text-xs text-zinc-500">
 							{att.key}
 						</h6>
 
