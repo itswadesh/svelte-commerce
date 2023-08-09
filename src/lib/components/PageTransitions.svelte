280882dbf578c2058f05d8776da3a0d7631e17a4
Swadesh Behera <itswadesh@gmail.com>
2023-08-09 13:33:29 +0530

    fix: login screen position

--- src/lib/components/PageTransitions.svelte
+++ src/lib/components/PageTransitions.svelte
@@ -27,7 +27,7 @@ const pageTransitionDuration = 500
 <div class="transition-outer">
 	{#key url}
 		<div
-			class="transition-inner flex flex-col items-center justify-center text-center"
+			class="transition-inner flex flex-col justify-center text-center"
 			transition:scale="{{ delay: 0, duration: 100, easing: quintOut }}"
 		>
 			<slot />
