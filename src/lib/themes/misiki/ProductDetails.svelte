b8b40113cfb51325ecf2f985263e368526a1ea78
pratapmaharana <pratapofficial1111@gmail.com>
2023-10-30 10:50:44 +0530

    add: specifications at misiki theme

--- src/lib/themes/misiki/ProductDetails.svelte
+++ src/lib/themes/misiki/ProductDetails.svelte
@@ -90,7 +90,7 @@ const cookies = Cookie()
 const isServer = import.meta.env.SSR
 
 export let data
-// console.log('zzzzzzzzzzzzzzzzzz', data)
+console.log('zzzzzzzzzzzzzzzzzz', data)
 
 let currentVariantId = $page.url.searchParams?.get('variant') || ''
 let currentVariantPrice = data.product?.price || 0
@@ -1277,6 +1277,46 @@ async function updateVariant(variant) {
 							Create Back Order
 						</button>
 					</form>
+				{/if}
+
+				<!-- Specifications -->
+
+				{#if data.product?.specifications?.length}
+					<div>
+						<div class="mb-2 flex items-center gap-2 uppercase">
+							<h5>Specifications</h5>
+
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1"
+								stroke="currentColor"
+								class="shrink-0 w-5 h-5">
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+								></path>
+							</svg>
+						</div>
+
+						<ul class="m-0 p-0 list-none flex flex-col gap-1">
+							{#each data.product?.specifications as s}
+								<li class="flex items-center gap-3 p-3 border">
+									<h6 class="w-24 sm:w-44 font-medium">
+										{s.name || '_'}
+									</h6>
+
+									:
+
+									<p class="flex-1">
+										{s.value || '_'}
+									</p>
+								</li>
+							{/each}
+						</ul>
+					</div>
 				{/if}
 
 				{#await data.streamed?.moreProductDetails then value}
