9984b0cbf80d75f87b4b39bbfddda68be6ab4900
pratapmaharana <pratapofficial1111@gmail.com>
2023-11-06 18:58:04 +0530

    fix: breadcrumb

--- src/lib/components/Breadcrumb.svelte
+++ src/lib/components/Breadcrumb.svelte
@@ -35,15 +35,17 @@ export { clazz as class }
 	<!-- Category Pool -->
 
 	{#each categoryPool as d, dx}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="h-5 w-5 px-1 shrink-0"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-			stroke-width="2">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
-		</svg>
+		{#if dx === 0}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5 px-1 shrink-0"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
+			</svg>
+		{/if}
 
 		<a
 			href="{navigateToProperPath(d.link || d.slug)}?sort=-updatedAt"
