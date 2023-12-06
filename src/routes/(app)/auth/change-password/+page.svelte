0c38ac4c67801b6fda2891584525efc99dc6d04b
pratapmaharana <pratapofficial1111@gmail.com>
2023-12-06 17:09:39 +0530

    add: zod validation at signup and change password

--- src/routes/(app)/auth/change-password/+page.svelte
+++ src/routes/(app)/auth/change-password/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 import { browser } from '$app/environment'
 import { goto, invalidateAll } from '$app/navigation'
+import { LazyImg, Error } from '$lib/components'
 import { page } from '$app/stores'
 import { post } from '$lib/utils/api'
-import { UserService } from '$lib/services'
-import { LazyImg, Error } from '$lib/components'
-
 import { TextboxFloating, PrimaryButton } from '$lib/ui'
+import { UserService } from '$lib/services'
+import { z } from 'zod'
 import SEO from '$lib/components/SEO/index.svelte'
 
 const seoProps = {
@@ -23,6 +23,34 @@ let oldPassowrdType = 'password'
 let newPassowrdType = 'password'
 let confirmationPassowrdType = 'password'
 let err
+let zodError = null
+
+const zodChangePasswordSchema = z
+	.object({
+		oldPassword: z
+			.string({ required_error: 'Old Password is required' })
+			.min(1, { message: 'Old Password must be at least 1 character' }),
+		password: z
+			.string({ required_error: 'Password is required' })
+			.min(1, { message: 'Password must be at least 1 character' }),
+		passwordConfirmation: z
+			.string({ required_error: 'Confirm Password is required' })
+			.min(1, { message: 'Confirm Password must be at least 1 character' })
+	})
+	.superRefine(({ password, passwordConfirmation }, ctx) => {
+		if (password !== passwordConfirmation) {
+			ctx.addIssue({
+				code: 'custom',
+				message: 'Password and Confirm Password must match',
+				path: ['password']
+			}),
+				ctx.addIssue({
+					code: 'custom',
+					message: 'Password and Confirm Password must match',
+					path: ['passwordConfirmation']
+				})
+		}
+	})
 
 function toggleCurrentPassowrd() {
 	showOldPassword = !showOldPassword
@@ -47,20 +75,36 @@ function toggleConfirmationPassowrd() {
 
 async function submit(p) {
 	try {
+		err = null
 		loading = true
-		const { oldPassword, password, passwordConfirmation } = p
 
-		const res = await UserService.changePasswordService({
-			oldPassword: oldPassword,
-			password: password,
-			passwordConfirmation: passwordConfirmation,
-			storeId: $page.data.storeId,
-			origin: $page.data.origin
-		})
+		try {
+			zodChangePasswordSchema.parse(p)
+			zodError = null
+		} catch (error) {
+			if (error instanceof z.ZodError) {
+				zodError = error.errors.reduce((acc, err) => {
+					acc[err.path[0]] = err.message
+					return acc
+				}, {})
+			}
+		}
 
-		await invalidateAll()
+		if (Object.keys(zodError).length === 0) {
+			const { oldPassword, password, passwordConfirmation } = p
 
-		if (browser) goto('/auth/change-success')
+			const res = await UserService.changePasswordService({
+				oldPassword: oldPassword,
+				password: password,
+				passwordConfirmation: passwordConfirmation,
+				storeId: $page.data.storeId,
+				origin: $page.data.origin
+			})
+
+			await invalidateAll()
+
+			if (browser) goto('/auth/change-success')
+		}
 	} catch (e) {
 		err = e
 	} finally {
@@ -92,157 +136,175 @@ async function submit(p) {
 
 	<h2 class="mb-8 w-full text-center text-2xl font-semibold text-primary-500">Change Password</h2>
 
-	<Error err="{err}" />
+	<Error {err} class="mb-5" />
 
 	<form class="mb-5 flex flex-col gap-5" on:submit|preventDefault="{() => submit(password)}">
-		<div class="relative">
-			<TextboxFloating
-				type="{oldPassowrdType}"
-				label="Old Password"
-				class="w-full"
-				required
-				bind:value="{password.oldPassword}" />
+		<div>
+			<div class="relative">
+				<TextboxFloating
+					type="{oldPassowrdType}"
+					label="Old Password"
+					class="w-full"
+					required
+					bind:value="{password.oldPassword}" />
 
-			<button
-				type="button"
-				tabindex="-1"
-				class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
-				on:click="{() => toggleCurrentPassowrd()}">
-				{#if showOldPassword}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-						></path>
-					</svg>
-				{:else}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-						></path>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-					</svg>
-				{/if}
-			</button>
+				<button
+					type="button"
+					tabindex="-1"
+					class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
+					on:click="{() => toggleCurrentPassowrd()}">
+					{#if showOldPassword}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+							></path>
+						</svg>
+					{:else}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+							></path>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+						</svg>
+					{/if}
+				</button>
+			</div>
+
+			{#if zodError?.oldPassword}
+				<p class="mt-1 text-red-600">{zodError?.oldPassword}</p>
+			{/if}
 		</div>
 
-		<div class="relative">
-			<TextboxFloating
-				type="{newPassowrdType}"
-				label="New Password"
-				class="w-full"
-				required
-				bind:value="{password.password}" />
+		<div>
+			<div class="relative">
+				<TextboxFloating
+					type="{newPassowrdType}"
+					label="New Password"
+					class="w-full"
+					required
+					bind:value="{password.password}" />
 
-			<button
-				type="button"
-				tabindex="-1"
-				class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
-				on:click="{() => toggleNewPassowrd()}">
-				{#if showNewPassword}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-						></path>
-					</svg>
-				{:else}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-						></path>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-					</svg>
-				{/if}
-			</button>
+				<button
+					type="button"
+					tabindex="-1"
+					class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
+					on:click="{() => toggleNewPassowrd()}">
+					{#if showNewPassword}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+							></path>
+						</svg>
+					{:else}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+							></path>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+						</svg>
+					{/if}
+				</button>
+			</div>
+
+			{#if zodError?.password}
+				<p class="mt-1 text-red-600">{zodError?.password}</p>
+			{/if}
 		</div>
 
-		<div class="relative">
-			<TextboxFloating
-				type="{confirmationPassowrdType}"
-				label="Confirm Password"
-				class="w-full"
-				required
-				bind:value="{password.passwordConfirmation}" />
+		<div>
+			<div class="relative">
+				<TextboxFloating
+					type="{confirmationPassowrdType}"
+					label="Confirm Password"
+					class="w-full"
+					required
+					bind:value="{password.passwordConfirmation}" />
 
-			<button
-				type="button"
-				tabindex="-1"
-				class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
-				on:click="{() => toggleConfirmationPassowrd()}">
-				{#if showConfirmationPassword}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-						></path>
-					</svg>
-				{:else}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-						></path>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-					</svg>
-				{/if}
-			</button>
+				<button
+					type="button"
+					tabindex="-1"
+					class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
+					on:click="{() => toggleConfirmationPassowrd()}">
+					{#if showConfirmationPassword}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+							></path>
+						</svg>
+					{:else}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+							></path>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+						</svg>
+					{/if}
+				</button>
+			</div>
+
+			{#if zodError?.passwordConfirmation}
+				<p class="mt-1 text-red-600">{zodError?.passwordConfirmation}</p>
+			{/if}
 		</div>
 
-		<PrimaryButton type="submit" loading="{loading}" class="w-full">UPDATE PASSWORD</PrimaryButton>
+		<PrimaryButton type="submit" {loading} class="w-full">UPDATE PASSWORD</PrimaryButton>
 	</form>
 
 	<div class="mx-auto flex max-w-max flex-col gap-1 text-center text-sm">
--- src/routes/(app)/auth/signup/+page.svelte
+++ src/routes/(app)/auth/signup/+page.svelte
@@ -6,12 +6,14 @@ import { page } from '$app/stores'
 import { post } from '$lib/utils/api'
 import { toast } from '$lib/utils'
 import { UserService } from '$lib/services'
+import { z } from 'zod'
 import Cookie from 'cookie-universal'
 import Error from '$lib/components/Error.svelte'
 import LazyImg from '$lib/components/Image/LazyImg.svelte'
 import PrimaryButton from '$lib/ui/PrimaryButton.svelte'
 import SEO from '$lib/components/SEO/index.svelte'
 import TextboxFloating from '$lib/ui/TextboxFloating.svelte'
+
 const IS_DEV = import.meta.env.DEV
 
 const cookies = Cookie()
@@ -37,6 +39,44 @@ let passwordType = 'password'
 let showConfirmPassword = false
 let confirmPasswordType = 'password'
 let err
+let zodError = null
+
+const zodSignupSchema = z
+	.object({
+		email: z
+			.string({ required_error: 'Email is required' })
+			.email({ message: 'Email must be a valid email address' }),
+		firstName: z
+			.string({ required_error: 'First Name is required' })
+			.min(3, { message: 'First Name must be at least 3 characters' }),
+		lastName: z
+			.string({ required_error: 'Last Name is required' })
+			.min(3, { message: 'Last Name must be at least 3 characters' }),
+		password: z
+			.string({ required_error: 'Password is required' })
+			.min(1, { message: 'Password must be at least 1 character' }),
+		passwordConfirmation: z
+			.string({ required_error: 'Confirm Password is required' })
+			.min(1, { message: 'Confirm Password must be at least 1 character' }),
+		phone: z
+			.string()
+			.min(10, { message: 'Phone must be at least 10 digits' })
+			.max(17, { message: 'Phone must be less then 17 digits' })
+	})
+	.superRefine(({ password, passwordConfirmation }, ctx) => {
+		if (password !== passwordConfirmation) {
+			ctx.addIssue({
+				code: 'custom',
+				message: 'Password and Confirm Password must match',
+				path: ['password']
+			}),
+				ctx.addIssue({
+					code: 'custom',
+					message: 'Password and Confirm Password must match',
+					path: ['passwordConfirmation']
+				})
+		}
+	})
 
 function togglePassword() {
 	showPassword = !showPassword
@@ -68,39 +108,55 @@ function getLoginUrl(baseUrl) {
 
 async function submit(n) {
 	try {
+		err = null
 		loading = true
-		const { firstName, lastName, phone, email, password, passwordConfirmation } = n
 
-		const res = await UserService.signupService({
-			firstName: firstName,
-			lastName: lastName,
-			phone: phone,
-			email: email,
-			password: password,
-			passwordConfirmation: passwordConfirmation,
-			role: role,
-			storeId: $page.data.storeId,
-			origin: $page.data.origin
-		})
-
-		const me = {
-			id: res._id || res.id,
-			email: res.email,
-			phone: res.phone,
-			firstName: res.firstName,
-			lastName: res.lastName,
-			avatar: res.avatar,
-			role: res.role,
-			verified: res.verified,
-			active: res.active,
-			store: res.store
+		try {
+			zodSignupSchema.parse(n)
+			zodError = null
+		} catch (error) {
+			if (error instanceof z.ZodError) {
+				zodError = error.errors.reduce((acc, err) => {
+					acc[err.path[0]] = err.message
+					return acc
+				}, {})
+			}
 		}
 
-		await cookies.set('me', me, { path: '/' })
-		// $page.data.me = me
-		await invalidateAll()
+		if (Object.keys(zodError).length === 0) {
+			const { firstName, lastName, phone, email, password, passwordConfirmation } = n
 
-		if (browser) goto('/')
+			const res = await UserService.signupService({
+				firstName: firstName,
+				lastName: lastName,
+				phone: phone,
+				email: email,
+				password: password,
+				passwordConfirmation: passwordConfirmation,
+				role: role,
+				storeId: $page.data.storeId,
+				origin: $page.data.origin
+			})
+
+			const me = {
+				id: res._id || res.id,
+				email: res.email,
+				phone: res.phone,
+				firstName: res.firstName,
+				lastName: res.lastName,
+				avatar: res.avatar,
+				role: res.role,
+				verified: res.verified,
+				active: res.active,
+				store: res.store
+			}
+
+			await cookies.set('me', me, { path: '/' })
+			// $page.data.me = me
+			await invalidateAll()
+
+			if (browser) goto('/')
+		}
 	} catch (e) {
 		toast(e, 'error')
 		err = e
@@ -133,33 +189,55 @@ async function submit(n) {
 
 	<h2 class="mb-8 w-full text-center text-2xl font-semibold text-primary-500">Signup</h2>
 
-	<Error err="{err}" />
+	<Error {err} />
 
 	<form class="mb-5 flex flex-col gap-5" on:submit|preventDefault="{() => submit(newResistration)}">
-		<TextboxFloating
-			type="text"
-			label="First Name"
-			class="w-full"
-			required
-			bind:value="{newResistration.firstName}" />
+		<div>
+			<TextboxFloating
+				type="text"
+				label="First Name"
+				class="w-full"
+				required
+				bind:value="{newResistration.firstName}" />
 
-		<TextboxFloating
-			type="text"
-			label="Last Name"
-			class="w-full"
-			required
-			bind:value="{newResistration.lastName}" />
+			{#if zodError?.firstName}
+				<p class="mt-1 text-red-600">{zodError?.firstName}</p>
+			{/if}
+		</div>
 
-		<TextboxFloating type="tel" label="Phone" class="w-full" bind:value="{newResistration.phone}" />
+		<div>
+			<TextboxFloating
+				type="text"
+				label="Last Name"
+				class="w-full"
+				required
+				bind:value="{newResistration.lastName}" />
+
+			{#if zodError?.lastName}
+				<p class="mt-1 text-red-600">{zodError?.lastName}</p>
+			{/if}
+		</div>
+
+		<div>
+			<TextboxFloating
+				type="tel"
+				label="Phone"
+				class="w-full"
+				bind:value="{newResistration.phone}" />
+
+			{#if zodError?.phone}
+				<p class="mt-1 text-red-600">{zodError?.phone}</p>
+			{/if}
+		</div>
 
 		<div>
 			<TextboxFloating
 				type="email"
 				label="Email"
-				class="mb-1 w-full"
+				class="w-full"
 				bind:value="{newResistration.email}" />
 
-			<p class="text-xs">
+			<p class="mt-1 text-xs">
 				Do not have email?
 				<a
 					href="{getLoginUrl('/auth/login')}"
@@ -168,107 +246,123 @@ async function submit(n) {
 					Signup using phone number
 				</a>
 			</p>
+
+			{#if zodError?.email}
+				<p class="mt-1 text-red-600">{zodError?.email}</p>
+			{/if}
 		</div>
 
-		<div class="relative">
-			<TextboxFloating
-				type="{passwordType}"
-				label="Password"
-				class="w-full"
-				required
-				bind:value="{newResistration.password}" />
+		<div>
+			<div class="relative">
+				<TextboxFloating
+					type="{passwordType}"
+					label="Password"
+					class="w-full"
+					required
+					bind:value="{newResistration.password}" />
 
-			<button
-				type="button"
-				tabindex="-1"
-				class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
-				on:click="{togglePassword}">
-				{#if showPassword}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-						></path>
-					</svg>
-				{:else}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-						></path>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-					</svg>
-				{/if}
-			</button>
+				<button
+					type="button"
+					tabindex="-1"
+					class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
+					on:click="{togglePassword}">
+					{#if showPassword}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+							></path>
+						</svg>
+					{:else}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+							></path>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+						</svg>
+					{/if}
+				</button>
+			</div>
+
+			{#if zodError?.password}
+				<p class="mt-1 text-red-600">{zodError?.password}</p>
+			{/if}
 		</div>
 
-		<div class="relative">
-			<TextboxFloating
-				type="{confirmPasswordType}"
-				label="Confirm Password"
-				class="w-full"
-				required
-				bind:value="{newResistration.passwordConfirmation}" />
+		<div>
+			<div class="relative">
+				<TextboxFloating
+					type="{confirmPasswordType}"
+					label="Confirm Password"
+					class="w-full"
+					required
+					bind:value="{newResistration.passwordConfirmation}" />
 
-			<button
-				type="button"
-				tabindex="-1"
-				class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
-				on:click="{toggleConfirmPassword}">
-				{#if showConfirmPassword}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-						></path>
-					</svg>
-				{:else}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-5 w-5">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-						></path>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-					</svg>
-				{/if}
-			</button>
+				<button
+					type="button"
+					tabindex="-1"
+					class="absolute inset-y-0 right-2 flex cursor-pointer items-end justify-center pb-2"
+					on:click="{toggleConfirmPassword}">
+					{#if showConfirmPassword}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+							></path>
+						</svg>
+					{:else}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="h-5 w-5">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+							></path>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+						</svg>
+					{/if}
+				</button>
+			</div>
+
+			{#if zodError?.passwordConfirmation}
+				<p class="mt-1 text-red-600">{zodError?.passwordConfirmation}</p>
+			{/if}
 		</div>
 
-		<PrimaryButton type="submit" loading="{loading}" class="w-full">SUMBIT</PrimaryButton>
+		<PrimaryButton type="submit" {loading} class="w-full">SUMBIT</PrimaryButton>
 	</form>
 
 	<div class="mx-auto mb-5 flex max-w-max flex-col gap-1 text-center">
