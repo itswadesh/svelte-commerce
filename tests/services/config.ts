d35468bf725a042eda9bd0d056774f245ed4b4af
Ashok <ashokmarmath8@gmail.com>
2023-12-11 18:24:48 +0530

    chore: address api test case

--- tests/services/config.ts
+++ tests/services/config.ts
@@ -1,3 +1,30 @@
 export const host = 'demo.litekart.in'
 export const API_URL = 'https://staging.litekart.in/api/'
 export const storeId = '6356502aca4ff28ed596cb1b'
+
+
+
+
+
+export const getCookie = async() => {
+
+    const data = {
+			email: 'hi@litekart.in',
+			password: 'litekart',
+			store: storeId
+		}
+
+		// console.log(JSON.stringify(data));
+
+		// const opts =
+
+		const resLogin = await fetch(`${API_URL}login`, {
+            method: 'POST',
+			body: JSON.stringify(data || {}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+         const cookie = resLogin.headers.getSetCookie()[0]
+	     return cookie
+}
