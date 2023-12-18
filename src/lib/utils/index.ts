6856ca188d2537a6c7e0bbee11eae290d62328ae
Ashok <ashokmarmath8@gmail.com>
2023-12-18 12:09:01 +0530

    _

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -264,3 +264,29 @@ export const navigateToProperPath = (url) => {
 	// 	return url.trim();
 	// }
 }
+
+
+
+
+
+export const shopifyInit = async ({ query, variables }) => {
+	try {
+		const result = await fetch(import.meta.env.VITE_SHOPIFY_API_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Shopify-Storefront-Access-Token': import.meta.env.VITE_SHOPIFY_STOREFRONT_API_TOKEN
+			},
+			body: JSON.stringify({ query, variables })
+		}).then((res) => res.json())
+		if (result.errors) {
+			console.log({ errors: result.errors })
+		} else if (!result || !result.data) {
+			console.log({ result })
+			return 'No results found.'
+		}
+		return result.data
+	} catch (error) {
+		console.log(error)
+	}
+}
