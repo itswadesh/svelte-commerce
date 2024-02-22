ce8b0eb15f249274776ee9bc1f08ad7529de3473
Swadesh Behera <itswadesh@gmail.com>
2024-02-22 09:46:54 +0530

    -

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -33,11 +33,11 @@ export const handleError: HandleServerError = ({ error, event }) => {
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
-	console.log('IS_DEV............', IS_DEV)
+	console.log('IS_DEV............', IS_DEV || dev)
 	try {
 		const url = new URL(event.request.url)
 		const host = url.host
-		const protocol = dev != true ? `https://` : `http://`
+		const protocol = IS_DEV || dev != true ? `https://` : `http://`
 		// This is required for vercel as it parse URL as http instead of https
 		event.locals.origin = protocol + host
 		event.locals.host = host
