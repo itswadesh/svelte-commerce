bde16def2edc931d95adbca7c9605a977fad25bd
Swadesh Behera <itswadesh@gmail.com>
2024-07-05 08:36:22 +0530

    -

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
 // import adapter from '@sveltejs/adapter-vercel'
 import adapter from '@sveltejs/adapter-auto'
-import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-			postcss: true,
-			preserve: ['ld+json']
-		})
-	],
+	preprocess: {
+		postcss: true,
+		preserve: ['ld+json']
+	},
 	kit: {
-		alias: {
-			'lib/*': 'src/lib/*'
-		},
 		adapter: adapter(),
 		csrf: {
 			checkOrigin: false
