626edd425a44a819c83fa9f6502345a0fe533a48
Swadesh Behera <itswadesh@gmail.com>
2024-11-08 13:00:38 +0530

    fix: order details for multi-vendor case

--- src/routes/(app)/my/orders/[id]/+page.svelte
+++ src/routes/(app)/my/orders/[id]/+page.svelte
@@ -58,336 +58,337 @@ onMount(() => {
 		<section>
 			<BackButton to="/my/orders?sort=-updatedAt" clazz="mb-5" />
 
-			<div class="mb-5 overflow-hidden rounded border sm:mb-10">
-				<div class="flex flex-wrap items-center justify-between border-b bg-zinc-100 px-5 py-3">
-					<h6>Order No : #{data.order?.orderNo}</h6>
+			{#each data.order as order}
+				<div class="mb-5 overflow-hidden rounded border sm:mb-10">
+					<div class="flex flex-wrap items-center justify-between border-b bg-zinc-100 px-5 py-3">
+						<h6>Order No : #{order?.orderNo}</h6>
 
-					<h6>Order Date : {date(data.order?.createdAt)}</h6>
-				</div>
+						<h6>Order Date : {date(order?.createdAt)}</h6>
+					</div>
 
-				<!-- Order detail  -->
+					<!-- Order detail  -->
 
-				<div class="grid grid-cols-1 divide-y lg:grid-cols-2 lg:divide-y-0 lg:divide-x">
-					<div class="col-span-1 flex flex-col divide-y">
-						{#if data.order?.items}
-							<div class="divide-y divide-dashed text-xs text-zinc-500">
-								{#each data.order?.items as item}
-									{#if item}
-										<div class="flex gap-2 p-5 lg:gap-5">
-											<a
-												href="{`/product/${item.slug}`}"
-												aria-label="Click to view the product details"
-												class="shrink-0">
-												{#if item.isCustomized}
-													<img
-														src="{item.customizedImg}"
-														alt=" "
-														class="h-auto w-14 object-contain object-top" />
-												{:else}
-													<LazyImg
-														src="{item.img}"
-														alt=" "
-														width="56"
-														class="h-auto w-14 object-contain object-top" />
-												{/if}
-											</a>
-
-											<div class="flex w-full flex-1 flex-col gap-0.5 xl:pr-4">
-												<div class="flex justify-between gap-2 sm:gap-4">
-													<a
-														href="{`/product/${item.slug}`}"
-														aria-label="Click to view the product details"
-														class="flex-1 hover:underline">
-														<p>
-															{item.name}
-														</p>
-													</a>
-
-													{#if $page.data.store?.isFnb && item.foodType}
-														<div>
-															{#if item.foodType === 'veg'}
-																<img src="{productVeg}" alt="veg" class="h-5 w-5" />
-															{:else if item.foodType === 'nonveg'}
-																<img src="{productNonVeg}" alt="non veg" class="h-5 w-5" />
-															{/if}
-														</div>
+					<div class="grid grid-cols-1 divide-y lg:grid-cols-2 lg:divide-y-0 lg:divide-x">
+						<div class="col-span-1 flex flex-col divide-y">
+							{#if order?.items}
+								<div class="divide-y divide-dashed text-xs text-zinc-500">
+									{#each order?.items as item}
+										{#if item}
+											<div class="flex gap-2 p-5 lg:gap-5">
+												<a
+													href="{`/product/${item.slug}`}"
+													aria-label="Click to view the product details"
+													class="shrink-0">
+													{#if item.isCustomized}
+														<img
+															src="{item.customizedImg}"
+															alt=" "
+															class="h-auto w-14 object-contain object-top" />
+													{:else}
+														<LazyImg
+															src="{item.img}"
+															alt=" "
+															width="56"
+															class="h-auto w-14 object-contain object-top" />
 													{/if}
-												</div>
+												</a>
 
-												{#if item.qty}
-													<span>
-														Qty :
+												<div class="flex w-full flex-1 flex-col gap-0.5 xl:pr-4">
+													<div class="flex justify-between gap-2 sm:gap-4">
+														<a
+															href="{`/product/${item.slug}`}"
+															aria-label="Click to view the product details"
+															class="flex-1 hover:underline">
+															<p>
+																{item.name}
+															</p>
+														</a>
 
-														<b>
-															{item.qty}
-														</b>
-													</span>
-												{/if}
-
-												{#if item.size}
-													<span>
-														Size :
-
-														<b>
-															{item.size}
-														</b>
-													</span>
-												{/if}
-
-												{#if item?.usedOptions?.length}
-													{#each item?.usedOptions as option}
-														{#if option?.val?.length && option?.val !== undefined && option?.val != ''}
-															<div class="flex flex-wrap gap-2">
-																<span>{option.name}:</span>
-
-																{#if option.val}
-																	<ul class="flex flex-wrap items-center gap-x-2 gap-y-1">
-																		{#each option.val as v, valIndex}
-																			{#if v}
-																				<b>
-																					{v}
-																				</b>
-
-																				{#if valIndex < option.val?.length - 1}
-																					,
-																				{/if}
-																			{/if}
-																		{/each}
-																	</ul>
+														{#if $page.data.store?.isFnb && item.foodType}
+															<div>
+																{#if item.foodType === 'veg'}
+																	<img src="{productVeg}" alt="veg" class="h-5 w-5" />
+																{:else if item.foodType === 'nonveg'}
+																	<img src="{productNonVeg}" alt="non veg" class="h-5 w-5" />
 																{/if}
 															</div>
 														{/if}
-													{/each}
-												{/if}
-
-												<div class="flex flex-wrap items-center gap-1">
-													Item price :
-
-													<span class="font-bold whitespace-nowrap text-zinc-800">
-														{currency(item.price, store?.currencySymbol)}
-													</span>
-
-													{#if item?.mrp > item?.price}
-														<span class="whitespace-nowrap text-zinc-500 line-through">
-															<strike>
-																{currency(item.mrp, store?.currencySymbol)}
-															</strike>
-														</span>
-
-														{#if Math.floor(((item.mrp - item.price) / item.mrp) * 100) > 0}
-															<span class="whitespace-nowrap text-secondary-500">
-																({Math.floor(((item.mrp - item.price) / item.mrp) * 100)}% off)
-															</span>
-														{/if}
-													{/if}
-												</div>
-
-												<div class="flex flex-wrap items-center gap-1">
-													Sub Total :
-
-													<span class="font-bold whitespace-nowrap text-zinc-800">
-														{currency(item.subtotal, store?.currencySymbol)}
-													</span>
-
-													{#if item?.total > item?.subtotal}
-														<span class="whitespace-nowrap text-zinc-500 line-through">
-															<strike>
-																{currency(item.total, store?.currencySymbol)}
-															</strike>
-														</span>
-
-														{#if Math.floor(((item.total - item.subtotal) / item.total) * 100) > 0}
-															<span class="whitespace-nowrap text-secondary-500">
-																({Math.floor(((item.total - item.subtotal) / item.total) * 100)}%
-																off)
-															</span>
-														{/if}
-													{/if}
-												</div>
-
-												{#if item?.files?.length}
-													<ul class="mt-2 p-0 list-none flex flex-col gap-1">
-														{#each item?.files as file, fx}
-															<li>
-																<a href="{file}" download>
-																	<PrimaryButton loadingringsize="xs" class="text-xs">
-																		Download File
-
-																		{#if item?.files?.length > 1}
-																			{fx + 1}
-																		{/if}
-																	</PrimaryButton>
-																</a>
-															</li>
-														{/each}
-													</ul>
-												{/if}
-
-												{#if item?.status === 'delivered'}
-													<div class="mt-2 xl:mt-0 xl:w-1/3">
-														<a
-															href="/my/reviews/create?pid={item?.pid}&oid={item?.orderItemId}&ref=/product/{item?.slug}"
-															aria-label="Click to visit rate & review product"
-															class="max-w-max whitespace-nowrap font-semibold text-indigo-500 focus:outline-none hover:underline">
-															Rate & Review Product
-														</a>
 													</div>
-												{/if}
+
+													{#if item.qty}
+														<span>
+															Qty :
+
+															<b>
+																{item.qty}
+															</b>
+														</span>
+													{/if}
+
+													{#if item.size}
+														<span>
+															Size :
+
+															<b>
+																{item.size}
+															</b>
+														</span>
+													{/if}
+
+													{#if item?.usedOptions?.length}
+														{#each item?.usedOptions as option}
+															{#if option?.val?.length && option?.val !== undefined && option?.val != ''}
+																<div class="flex flex-wrap gap-2">
+																	<span>{option.name}:</span>
+
+																	{#if option.val}
+																		<ul class="flex flex-wrap items-center gap-x-2 gap-y-1">
+																			{#each option.val as v, valIndex}
+																				{#if v}
+																					<b>
+																						{v}
+																					</b>
+
+																					{#if valIndex < option.val?.length - 1}
+																						,
+																					{/if}
+																				{/if}
+																			{/each}
+																		</ul>
+																	{/if}
+																</div>
+															{/if}
+														{/each}
+													{/if}
+
+													<div class="flex flex-wrap items-center gap-1">
+														Item price :
+
+														<span class="font-bold whitespace-nowrap text-zinc-800">
+															{currency(item.price, store?.currencySymbol)}
+														</span>
+
+														{#if item?.mrp > item?.price}
+															<span class="whitespace-nowrap text-zinc-500 line-through">
+																<strike>
+																	{currency(item.mrp, store?.currencySymbol)}
+																</strike>
+															</span>
+
+															{#if Math.floor(((item.mrp - item.price) / item.mrp) * 100) > 0}
+																<span class="whitespace-nowrap text-secondary-500">
+																	({Math.floor(((item.mrp - item.price) / item.mrp) * 100)}% off)
+																</span>
+															{/if}
+														{/if}
+													</div>
+
+													<div class="flex flex-wrap items-center gap-1">
+														Sub Total :
+
+														<span class="font-bold whitespace-nowrap text-zinc-800">
+															{currency(item.subtotal, store?.currencySymbol)}
+														</span>
+
+														{#if item?.total > item?.subtotal}
+															<span class="whitespace-nowrap text-zinc-500 line-through">
+																<strike>
+																	{currency(item.total, store?.currencySymbol)}
+																</strike>
+															</span>
+
+															{#if Math.floor(((item.total - item.subtotal) / item.total) * 100) > 0}
+																<span class="whitespace-nowrap text-secondary-500">
+																	({Math.floor(((item.total - item.subtotal) / item.total) * 100)}%
+																	off)
+																</span>
+															{/if}
+														{/if}
+													</div>
+
+													{#if item?.files?.length}
+														<ul class="mt-2 p-0 list-none flex flex-col gap-1">
+															{#each item?.files as file, fx}
+																<li>
+																	<a href="{file}" download>
+																		<PrimaryButton loadingringsize="xs" class="text-xs">
+																			Download File
+
+																			{#if item?.files?.length > 1}
+																				{fx + 1}
+																			{/if}
+																		</PrimaryButton>
+																	</a>
+																</li>
+															{/each}
+														</ul>
+													{/if}
+
+													{#if item?.status === 'delivered'}
+														<div class="mt-2 xl:mt-0 xl:w-1/3">
+															<a
+																href="/my/reviews/create?pid={item?.pid}&oid={item?.orderItemId}&ref=/product/{item?.slug}"
+																aria-label="Click to visit rate & review product"
+																class="max-w-max whitespace-nowrap font-semibold text-indigo-500 focus:outline-none hover:underline">
+																Rate & Review Product
+															</a>
+														</div>
+													{/if}
+												</div>
 											</div>
+										{/if}
+									{/each}
+								</div>
+
+								<div class="p-5 flex justify-end">
+									<div class="flex max-w-max flex-col items-start gap-2">
+										<p class="flex items-center">
+											<span class="mr-2 w-32">Subtotal</span>
+
+											<span>
+												: &nbsp; {currency(order?.amount?.subtotal, store?.currencySymbol)}
+											</span>
+										</p>
+
+										<p class="flex items-center">
+											<span class="mr-2 w-32">Discount</span>
+
+											<span>
+												: &nbsp;
+
+												{currency(order?.amount?.discount, store?.currencySymbol)}
+											</span>
+										</p>
+
+										{#if order?.coupon?.code}
+											<p class="flex items-center">
+												<span class="mr-2 w-32">Applied Coupon</span>
+
+												<span>
+													: &nbsp;
+													{order?.coupon?.code}
+												</span>
+											</p>
+										{/if}
+
+										<p class="flex items-center">
+											<span class="mr-2 w-32">Shipping</span>
+
+											<span>
+												: &nbsp;
+												{#if order?.amount?.shipping}
+													{currency(order?.amount?.shipping, store?.currencySymbol)}
+												{:else}
+													Free
+												{/if}
+											</span>
+										</p>
+
+										{#if order?.amount?.cod_charges}
+											<p class="flex items-center">
+												<span class="mr-2 w-32">COD Charges</span>
+
+												<span>
+													: &nbsp;
+
+													{currency(order?.amount?.cod_charges, store?.currencySymbol)}
+												</span>
+											</p>
+										{/if}
+
+										<hr class="w-full border-t border-zinc-200" />
+
+										<div class="flex items-center text-sm font-bold text-zinc-800">
+											<span class="mr-2 w-32">Total</span>
+
+											<span>
+												: &nbsp; {currency(order?.amount?.total, store?.currencySymbol)}
+											</span>
 										</div>
-									{/if}
-								{/each}
-							</div>
-
-							<div class="p-5 flex justify-end">
-								<div class="flex max-w-max flex-col items-start gap-2">
-									<p class="flex items-center">
-										<span class="mr-2 w-32">Subtotal</span>
-
-										<span>
-											: &nbsp; {currency(data.order?.amount?.subtotal, store?.currencySymbol)}
-										</span>
-									</p>
-
-									<p class="flex items-center">
-										<span class="mr-2 w-32">Discount</span>
-
-										<span>
-											: &nbsp;
-
-											{currency(data.order?.amount?.discount, store?.currencySymbol)}
-										</span>
-									</p>
-
-									{#if data.order?.coupon?.code}
-										<p class="flex items-center">
-											<span class="mr-2 w-32">Applied Coupon</span>
-
-											<span>
-												: &nbsp;
-												{data.order?.coupon?.code}
-											</span>
-										</p>
-									{/if}
-
-									<p class="flex items-center">
-										<span class="mr-2 w-32">Shipping</span>
-
-										<span>
-											: &nbsp;
-											{#if data.order?.amount?.shipping}
-												{currency(data.order?.amount?.shipping, store?.currencySymbol)}
-											{:else}
-												Free
-											{/if}
-										</span>
-									</p>
-
-									{#if data.order?.amount?.cod_charges}
-										<p class="flex items-center">
-											<span class="mr-2 w-32">COD Charges</span>
-
-											<span>
-												: &nbsp;
-
-												{currency(data.order?.amount?.cod_charges, store?.currencySymbol)}
-											</span>
-										</p>
-									{/if}
-
-									<hr class="w-full border-t border-zinc-200" />
-
-									<div class="flex items-center text-sm font-bold text-zinc-800">
-										<span class="mr-2 w-32">Total</span>
-
-										<span>
-											: &nbsp; {currency(data.order?.amount?.total, store?.currencySymbol)}
-										</span>
 									</div>
 								</div>
+							{:else}
+								<p>No order items found</p>
+							{/if}
+						</div>
+
+						<div class="col-span-1 flex flex-col gap-5 p-5 lg:gap-10">
+							<div>
+								<h5 class="mb-2">Delivery Address</h5>
+
+								<p class="flex flex-col">
+									<span>
+										{order?.shipping_address?.firstName}
+										{order?.shipping_address?.lastName}
+
+										<br />
+
+										{order?.shipping_address?.address}, {order?.shipping_address?.city},
+										{order?.shipping_address?.country}, {order?.shipping_address?.state}
+
+										<br />
+
+										{order?.shipping_address?.zip}
+									</span>
+								</p>
+
+								{#if order?.shipping_address?.phone}
+									<p>
+										{order?.shipping_address?.phone}
+									</p>
+								{/if}
 							</div>
-						{:else}
-							<p>No order items found</p>
-						{/if}
-					</div>
 
-					<div class="col-span-1 flex flex-col gap-5 p-5 lg:gap-10">
-						<div>
-							<h5 class="mb-2">Delivery Address</h5>
+							<div>
+								<h5 class="mb-2">Billing Address</h5>
 
-							<p class="flex flex-col">
-								<span>
-									{data.order?.shipping_address?.firstName}
-									{data.order?.shipping_address?.lastName}
+								<p class="flex flex-col">
+									<span>
+										{order?.billing_address?.firstName}
+										{order?.billing_address?.lastName}
 
-									<br />
+										<br />
 
-									{data.order?.shipping_address?.address}, {data.order?.shipping_address?.city},
-									{data.order?.shipping_address?.country}, {data.order?.shipping_address?.state}
+										{order?.billing_address?.address}, {order?.billing_address?.city},
+										{order?.billing_address?.country}, {order?.billing_address?.state}
 
-									<br />
+										<br />
 
-									{data.order?.shipping_address?.zip}
-								</span>
-							</p>
-
-							{#if data.order?.shipping_address?.phone}
-								<p>
-									{data.order?.shipping_address?.phone}
+										{order?.billing_address?.zip}
+									</span>
 								</p>
-							{/if}
-						</div>
 
-						<div>
-							<h5 class="mb-2">Billing Address</h5>
+								{#if order?.billing_address?.phone}
+									<p>
+										{order?.billing_address?.phone}
+									</p>
+								{/if}
+							</div>
 
-							<p class="flex flex-col">
-								<span>
-									{data.order?.billing_address?.firstName}
-									{data.order?.billing_address?.lastName}
-
-									<br />
-
-									{data.order?.billing_address?.address}, {data.order?.billing_address?.city},
-									{data.order?.billing_address?.country}, {data.order?.billing_address?.state}
-
-									<br />
-
-									{data.order?.billing_address?.zip}
-								</span>
-							</p>
-
-							{#if data.order?.billing_address?.phone}
-								<p>
-									{data.order?.billing_address?.phone}
-								</p>
-							{/if}
-						</div>
-
-						<!-- <div>
+							<!-- <div>
 							<h5 class="mb-2">Vendor Details :</h5>
 
 							<p class="flex flex-col">
 								<span>
-									{data.order?.vendorBusinessName},
+									{order?.vendorBusinessName},
 
-									{data.order?.vendorAddress?.address}, {data.order?.vendorAddress?.town},
+									{order?.vendorAddress?.address}, {order?.vendorAddress?.town},
 
-									{data.order?.vendorAddress?.city}, {data.order?.vendorAddress?.state}</span>
+									{order?.vendorAddress?.city}, {order?.vendorAddress?.state}</span>
 
-								<span>{data.order?.vendorAddress?.zip}</span>
+								<span>{order?.vendorAddress?.zip}</span>
 							</p>
 
-							{#if data.order?.vendorPhone}
+							{#if order?.vendorPhone}
 								<h6 class="mt-2">
-									Phone number: <span> {data.order?.vendorPhone}</span>
+									Phone number: <span> {order?.vendorPhone}</span>
 								</h6>
 							{/if}
 						</div> -->
+						</div>
 					</div>
 				</div>
-			</div>
-
+			{/each}
 			<!-- Order Tracker -->
 
 			<div>
