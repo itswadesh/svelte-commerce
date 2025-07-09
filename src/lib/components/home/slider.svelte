fac161f8fb166984b21b6fb620bdc18f64b7bc6e
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2025-07-09 20:40:36 +0530

    ui: use newsTicker plugin

--- src/lib/components/home/slider.svelte
+++ src/lib/components/home/slider.svelte
@@ -3,11 +3,11 @@
 	import type { CarouselAPI } from '../ui/carousel/context'
 	import AutoScroll from 'embla-carousel-auto-scroll'
 	import EmblaCarousel from 'embla-carousel'
+  import { page } from '$app/state'
 
 	let carouselApi: CarouselAPI | null = $state(null)
 	let emblaNode: HTMLDivElement | null = $state(null)
-
-	const text = $state('Assured freebies on orders over $125')
+  const newsTickerPlugin = $derived(page.data?.store?.plugins?.newsTicker)
 
 	onMount(() => {
 		if (!emblaNode) return console.log('skipping slider')
@@ -26,20 +26,22 @@
 </script>
 
 
-<div class="relative w-full">
-	<div bind:this={emblaNode} class="embla flex h-12 w-full gap-4 bg-black text-white">
-		<div class="embla__container">
-			{#each { length: 10 } as _, idx}
-				<div class="embla__slide flex items-center justify-between whitespace-nowrap text-center">
-					<p>{text}</p>
-					<div class="flex flex-grow justify-evenly">
-						<div class="h-[1px] w-[60%] border-[2px] border-b border-gray-300"></div>
-					</div>
-				</div>
-			{/each}
-		</div>
-	</div>
-</div>
+{#if newsTickerPlugin.active}
+  <div class="relative w-full">
+  	<div bind:this={emblaNode} class="embla flex h-12 w-full gap-4 bg-black text-white">
+  		<div class="embla__container">
+  			{#each { length: 10 } as _, idx}
+  				<div class="embla__slide flex items-center justify-between whitespace-nowrap text-center">
+  					<p>{@html newsTickerPlugin.html}</p>
+  					<div class="flex flex-grow justify-evenly">
+  						<div class="h-[1px] w-[60%] border-[2px] border-b border-gray-300"></div>
+  					</div>
+  				</div>
+  			{/each}
+  		</div>
+  	</div>
+  </div>
+{/if}
 
 <style>
 	.embla {
