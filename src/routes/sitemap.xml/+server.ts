26b3e600335f8ef0c78f597b63d5d68fb7a86e81
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2026-05-21 21:34:36 +0530

    feat: Sitemap redirection from core

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,12 +1,4 @@
-import { StoreService } from '@misiki/litekart-connector'
-import { error, redirect, type RequestHandler } from '@sveltejs/kit'
+import { redirectToSitemap } from "@misiki/kitcommerce-core/request-handlers";
+import { redirect } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async ({ fetch, cookies, url }) => {
-  const storeId = cookies.get('litekart_store_id')
-  const storeService = new StoreService(fetch)
-  const data = await storeService.get(`/api/stores/${storeId}`)
-  if (data?.sitemap)
-	  redirect(302, data.sitemap)
-  else
-    error(404, 'Sitemap not found, you can generate sitemap through the admin dashboard.')
-}
+export const GET = redirectToSitemap(redirect)
