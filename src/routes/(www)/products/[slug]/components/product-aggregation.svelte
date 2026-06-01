3e6932b2c10d8a8ef48e060060d88fb06a5e0875
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2026-06-01 20:17:00 +0530

    ui: Match aggregations with variations

--- src/routes/(www)/products/[slug]/components/product-aggregation.svelte
+++ src/routes/(www)/products/[slug]/components/product-aggregation.svelte
@@ -1,39 +1,42 @@
 <script lang="ts">
 	import { page } from '$app/state'
 	import { useProductState } from '$lib/core/composables/index.js'
-	import { ChartNoAxesGanttIcon } from '@lucide/svelte'
 	import { Button } from '$lib/components/ui/button'
 
 	const productState = useProductState()
 </script>
 
 {#if page.data?.product?.ag && Object.keys(page.data?.product?.ag).length}
-	<div class="">
-		<div class="intra-gap flex items-center">
-			<span class="text-sm text-gray-500">Select Options</span>
-			<ChartNoAxesGanttIcon class="h-3 w-3" />
-		</div>
+	<div class="intra-gap flex flex-col">
+		{#each Object.entries(page.data?.product?.ag || {}) as [optionName, values]}
+			{#if Array.isArray(values)}
+				<div class="flex flex-col gap-3">
+					<div class="flex items-center gap-2">
+						<span class="text-sm font-semibold text-gray-900 dark:text-gray-100">
+							{optionName}
 
-		<div class="my-2 flex flex-col flex-wrap justify-center gap-2">
-			{#each Object.entries(page.data?.product?.ag || {}) as [optionName, values]}
-				{#if Array.isArray(values)}
-					<div class="flex flex-col gap-1">
-						<span class="text-lg uppercase">{optionName}</span>
-						<div class="flex flex-wrap items-center gap-2">
-							{#each [...values].sort() as value}
-								<Button
-									variant={productState.selectedAggregations?.[optionName] === value ? 'default' : 'outline'}
-									class="relative overflow-hidden px-4 py-2"
-									disabled={!productState.isAggregationAvaliable(optionName, value)}
-									onclick={() => productState.toggleAggregation(optionName, value)}
-								>
-									{value}
-								</Button>
-							{/each}
-						</div>
+							{#if productState.selectedAggregations?.[optionName]}
+								:
+							{/if}
+						</span>
+
+						<span class="font-semibold">{productState.selectedAggregations?.[optionName] || ''}</span>
 					</div>
-				{/if}
-			{/each}
-		</div>
+
+					<div class="flex flex-wrap items-center gap-3">
+						{#each [...values].sort() as value}
+							<Button
+								variant={productState.selectedAggregations?.[optionName] === value ? 'default' : 'plain'}
+								disabled={!productState.isAggregationAvaliable(optionName, value)}
+								class={`min-w-[3.5rem] px-4 py-2 !bg-primary ${productState.selectedAggregations?.[optionName] === value ? '!bg-accent text-accent-foreground' : '!bg-transparent border !border-accent'}`}
+								onclick={() => productState.toggleAggregation(optionName, value)}
+							>
+								{value}
+							</Button>
+						{/each}
+					</div>
+				</div>
+			{/if}
+		{/each}
 	</div>
 {/if}
