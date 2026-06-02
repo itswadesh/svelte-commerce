ed1e286fb4062a2b38c8fbb18189aca4f0321683
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2026-06-02 17:10:50 +0530

    feat: Redirect from my pages

--- src/routes/(my)/+layout.server.ts
+++ src/routes/(my)/+layout.server.ts
@@ -1,1 +1,16 @@
-export { myLayoutServer as load } from '$lib/core/load-functions/index.js'
+//export { myLayoutServer as load } from '$lib/core/load-functions/index.js'
+
+import { redirect } from '@sveltejs/kit'
+import type { LayoutServerLoad } from './$types.js'
+
+export const load: LayoutServerLoad = async ({ fetch, url, cookies }) => {
+	const sid = cookies.get('connect.sid')
+	if (!sid) {
+    const searchParams = new URLSearchParams()
+    searchParams.set('show_auth', 'true')
+    searchParams.set('login', 'true')
+    searchParams.set('redirect', url.pathname + '?' + url.searchParams.toString())
+    redirect(307, '/?' + searchParams.toString())
+  }
+}
+
