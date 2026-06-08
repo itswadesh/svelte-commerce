6eaa51f400310b3a95b5d4a11306b5900cf6dda8
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2026-06-08 16:49:23 +0530

    fix: add view more to featured categories

--- src/lib/components/page-blocks/blocks/featured-categories.svelte
+++ src/lib/components/page-blocks/blocks/featured-categories.svelte
@@ -36,12 +36,14 @@
 					<p class="mt-4 text-sm font-medium text-muted-foreground">{block.metadata.subtitle}</p>
 				{/if}
 			</div>
-			<Button href="/categories" class="group">
-				View all categories
-				<svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-				</svg>
-			</Button>
+			{#if block.metadata.showViewMore}
+				<Button href={block.metadata.redirectsTo || "/categories"} class="group">
+					{block.metadata.viewMoreText}
+					<svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+					</svg>
+				</Button>
+			{/if}
 		</div>
 	{/if}
 
