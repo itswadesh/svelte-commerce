76504b1edbfe09f30c1d9c4476e048bb4507404f
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2026-06-08 17:49:20 +0530

    ui: collectio block redesign

--- src/lib/components/page-blocks/blocks/collection.svelte
+++ src/lib/components/page-blocks/blocks/collection.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
 	import Skeleton from '$lib/components/ui/skeleton/skeleton.svelte'
-	import LazyImg from '$lib/core/components/image/lazy-img.svelte'
 	import { getCollectionState } from '$lib/core/stores/collection.svelte.js'
 
 	const { block } = $props()
 
 	const collectionState = getCollectionState()
 	const collection = $derived(collectionState.getOneById(block.entityId))
+
+	const [aspectWidth, aspectHeight] = $derived(block.metadata.aspectRatio?.split(':') || ['1', '1'])
 </script>
 
-<div class="w-full">
-	{#if collectionState.loading}
-		<Skeleton />
-	{:else if collection}
-		<LazyImg aspectRatio={block.metadata.aspectRatio} src={collection.img} />
-		<p>
-			{collection.name}
-		</p>
-	{/if}
-</div>
+{#if collectionState.loading}
+	<Skeleton />
+{:else if collection}
+	<div
+		style="aspect-ratio: {aspectWidth}/{aspectHeight}; {block.metadata.maxWidth ? `max-width: ${block.metadata.maxWidth}px;` : ``} {block.metadata
+			.maxHeight
+			? ` max-height: ${block.metadata.maxHeight}px;`
+			: ``}"
+		class="flex h-full items-center justify-center"
+	>
+		<img src={collection.img} class="h-full object-contain" alt="" />
+	</div>
+{/if}
