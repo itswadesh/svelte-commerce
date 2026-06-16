1a0bbfb1527a7a6e9949584d8e41f7d3b199b688
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2026-06-16 18:46:33 +0530

    fix: featured category dynamic aspect ratio

--- src/lib/components/page-blocks/blocks/featured-categories.svelte
+++ src/lib/components/page-blocks/blocks/featured-categories.svelte
@@ -2,8 +2,8 @@
 	import { categoryService, type Category } from '$lib/core/services/index.js'
 	import { onMount } from 'svelte'
 	import LazyImg from '$lib/core/components/image/lazy-img.svelte'
-	import { Skeleton } from '$lib/components/ui/skeleton'
-	import { Button } from '$lib/components/ui/button'
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js'
+	import { Button } from '$lib/components/ui/button/index.js'
 	import { toast } from '@misiki/kitcommerce-core'
 
 	const { block } = $props()
@@ -37,7 +37,7 @@
 				{/if}
 			</div>
 			{#if block.metadata.showViewMore}
-				<Button href={block.metadata.redirectsTo || "/categories"} class="group">
+				<Button href={block.metadata.redirectsTo || '/categories'} class="group">
 					{block.metadata.viewMoreText}
 					<svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
@@ -47,7 +47,7 @@
 		</div>
 	{/if}
 
-	<div class="grid gap-2 px-2" style="grid-template-columns: repeat({block.metadata.columnCount || 5}, 1fr);">
+	<div class="grid gap-2" style="grid-template-columns: repeat({block.metadata.columnCount || 5}, 1fr);">
 		{#if loadingForCategory}
 			{#each Array(6) as _}
 				<div class="flex flex-col items-center">
@@ -56,14 +56,21 @@
 				</div>
 			{/each}
 		{:else}
-			{#each categories as { slug, icon, color, name, link, thumbnail, parentCategoryId } (slug)}
+			{#each categories as { slug, name, link, thumbnail } (slug)}
 				<a href={link ? link : slug ? `/${slug}` : `/products`} class="group flex flex-col items-center focus:outline-none">
-					<div class="relative aspect-square w-full overflow-hidden bg-muted bg-red-200 shadow-sm transition-all duration-500 ease-out">
-						<LazyImg src={thumbnail} alt={name} class="h-full w-full object-cover transition-transform duration-700 ease-in-out" />
+					<div class="relative w-full overflow-hidden bg-muted shadow-sm transition-all duration-500 ease-out">
+						<LazyImg
+							src={thumbnail || ''}
+							aspectRatio={block.metadata.aspectRatio}
+							alt={name}
+							class="h-full w-full object-contain transition-transform duration-700 ease-in-out"
+						/>
 					</div>
-					<span class="mt-2 px-2 text-center text-sm font-bold tracking-tight text-foreground transition-colors duration-300 lg:text-base">
-						{name}
-					</span>
+					{#if !block?.metadata?.hideCategoryName}
+						<span class="mt-2 px-2 text-center text-sm font-bold tracking-tight text-foreground transition-colors duration-300 lg:text-base">
+							{name}
+						</span>
+					{/if}
 				</a>
 			{/each}
 		{/if}
